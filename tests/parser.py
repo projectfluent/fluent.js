373cd8b07a9ed58e70b42a4f6b594bd1f3b73378
Zbigniew Braniecki <gandalf@mozilla.com>
2011-06-15 19:32:31 +0200

    add basic attribute tests

--- tests/parser.py
+++ tests/parser.py
@@ -50,12 +50,32 @@ class L20nParserTestCase(unittest.TestCase):
             "<id value'",
             "<id'value'>",
             '<id"value">',
-            '< id "value">'
+            '< id "value">',
+            '<()>',
+            '<+s>',
         ]
         for string in strings:
             self.assertRaises(ParserError, self.parser.parse, string)
 
+    def test_basic_attributes(self):
+        string = "<id attr1: 'foo'>"
+        lol = self.parser.parse(string)
+        self.assertEqual(len(lol.body[0].attrs), 1)
+        attr = lol.body[0].attrs[0]
+        self.assertEqual(attr.key.name, "attr1")
+        self.assertEqual(attr.value.content, "foo")
 
+    def test_attribute_errors(self):
+        strings = [
+            '<id : "foo">',
+            "<id 2: >",
+            "<id a: >",
+            "<id: ''>",
+            "<id a: b:>",
+            "<id a: 'foo' 'heh'>",
+        ]
+        for string in strings:
+            self.assertRaises(ParserError, self.parser.parse, string)
 
 if __name__ == '__main__':
     unittest.main()
