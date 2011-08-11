11b5a0512f57c2fa870e938dd4cf0a192135afe7
Zbigniew Braniecki <gandalf@mozilla.com>
2011-08-11 10:07:10 -0700

    add basic js binding

---
+++ js/l20n.js
@@ -0,0 +1,74 @@
+
+var L20n = {
+  xul: {},
+  cache: {},
+  getContext: function(path) {
+    return new L20n.Context(this.xul[path], this.cache)
+  },
+  hasContext: function(path) {
+    return this.xul[path]?true:false;
+  },
+  createContext: function(path, testPath) {
+    this.xul[path] = {'l20n': {}, 'data': {}};
+    return new L20n.Context(this.xul[path], this.cache);
+  },
+}
+
+L20n.Context = function(obj, cache) {
+  this.obj = obj;
+  this.curObj = this.obj['l20n'];
+  this.cache = cache;
+}
+
+L20n.Context.prototype = {
+  _getEntity: function(id, params) {
+    var localObj;
+    if (params) {
+      localObj = {};
+      for (var i in params) {
+        localObj[i] = params[i];
+      }
+      localObj.__proto__ = this.curObj;
+    } else {
+      localObj = this.curObj;
+    }
+    return [localObj[id], localObj]; 
+  },
+  get: function(id, params) {
+    let [entity, localObj] = this._getEntity(id, params);
+    return localObj.getent(localObj, id)
+  },
+  getAttributes: function(id, params) {
+    let attrs = {};
+    let [entity, localObj] = this._getEntity(id, params);
+    for (let i in entity._attrs) {
+      if (typeof(entity._attrs[i])=='function') {
+        attrs[i] = entity._attrs[i](localObj);
+      } else {
+        attrs[i] = entity._attrs[i];
+      }
+    }
+    return attrs;
+  },
+  addReference: function(uri) {
+ 	var read = function(data) {
+ 	  eval(data);
+ 	}
+
+    if (!this.cache[uri]) {
+      var data = this._get_file(uri);
+      this.cache[uri] = {}
+      read.apply(this.cache[uri], Array(data))
+    }
+    var i;
+    for (i in this.cache[uri]) {
+      this.obj['l20n'][i] = this.cache[uri][i];
+    }
+  },
+  set data(val) {
+    this.obj['data'] = val;
+    this.obj['data'].__proto__ = this.obj['l20n'];
+    this.curObj = this.obj['data'];
+  }
+}
+
