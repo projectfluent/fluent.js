f7d3decbe086643988506632faa03b41aab8ea1e
Zbigniew Braniecki <gandalf@mozilla.com>
2011-08-11 18:05:26 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+import os
 
 setup(name='l20n',
       version='1.0',
@@ -8,6 +9,12 @@ setup(name='l20n',
       author='Zbigniew Braniecki',
       author_email='zbigniew@braniecki.net',
       url='https://github.com/zbraniecki/pyl20n',
-      packages=['l20n', 'l20n.compiler'],
+      packages=['l20n',
+                'l20n.compiler',
+                'l20n.format',
+                'l20n.format.lol'],
+      package_dir = {
+          'l20n': os.path.join('python', 'l20n'),
+      }
      )
 
