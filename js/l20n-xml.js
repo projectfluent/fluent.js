c48edb328697fe1da9bc1d5eb82227d71e423f90
Zbigniew Braniecki <gandalf@mozilla.com>
2012-01-18 21:03:55 +0100

    more code cleanup in l20n-xml.js

--- js/l20n-xml.js
+++ js/l20n-xml.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
   var links = headNode.getElementsByTagName('link')
   for (var i = 0; i < links.length; i++) {
-    if (links[i].type 'intl/l20n')
+    if (links[i].getAttribute('type') == 'intl/l20n')
       ctx.addResource(links[i].getAttribute('href'))
   }
 
@@ -46,27 +46,27 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 
   ctx.freeze();
-}, false);
+});
 
 function getPathTo(element, context) {
-  if (element === context) {
-    return '.';
-  }
-  var id = element.getAttribute('id');
-  if (id) {
-    return '*[@id="' + id + '"]';
-  }
+  const TYPE_ELEMENT = 1;
 
-  var ix = 0;
+  if (element === context)
+    return '.';
+
+  var id = element.getAttribute('id');
+  if (id)
+    return '*[@id="' + id + '"]';
+
+  var index = 0;
   var siblings = element.parentNode.childNodes;
-  for (var i=0, sibling; sibling = siblings[i]; i++) {
+  for (var i = 0, sibling; sibling = siblings[i]; i++) {
     if (sibling === element) {
       var pathToParent = getPathTo(element.parentNode, context);
-      return pathToParent + '/' + element.tagName + '[' + (ix + 1) + ']';
+      return pathToParent + '/' + element.tagName + '[' + (index + 1) + ']';
     }
-    if (sibling.nodeType === 1 && sibling.tagName === element.tagName) {
-      ix++;
-    }
+    if (sibling.nodeType === TYPE_ELEMENT && sibling.tagName === element.tagName)
+      index++;
   }
 }
 
@@ -78,61 +78,62 @@ function getElementByPath(path, context) {
 
 
 function localizeNode(ctx, node) {
-  var l10nId;
-  if (l10nId = node.getAttribute('l10n-id')) {
-    var args;
-    // node.nodeData 
-    // must not be exposed
-    if (node.nodeData) {
-      args = node.nodeData;
-    } else if (node.hasAttribute('l10n-args')) {
-      args = JSON.parse(node.getAttribute('l10n-args'));
-      node.nodeData = args;
-    }
-    // get attributes from the LO
-    var attrs = ctx.getAttributes(l10nId, args);
-    if (attrs) {
-      for (var j in attrs) {
-        node.setAttribute(j, attrs[j]);
-      }
-    }
-    var valueFromCtx = ctx.get(l10nId, args);
-    if (valueFromCtx === null) {
-      return;
-    }
-    // deep-copy the original node
-    var origNode = node.cloneNode(true);
-    node.innerHTML = valueFromCtx;
-    // overlay the attributes of descendant nodes
-    var children = node.getElementsByTagName('*');
-    for (var j=0, child; child = children[j]; j++) {
-      // Match the child node with the equivalent node in origNode.
-      // The tricky part is that the node in origNode might have been 
-      // translated before; if so and if it had been reordered via 
-      // `l10n-path`, it's likely that even though the path points to it, it 
-      // actually _isn't_ the right node.
-      var origChild;
-      var path = child.getAttribute('l10n-path');
-      if ( ! path) {
-        path = getPathTo(child, node);
-      }
-      origChild = getElementByPath(path, origNode);
-      // If the origChild already has `l10n-path`, it's likely that it has
-      // already been translated once.  Follow its `l10n-path` again to find 
-      // the true source equivalent in origNode.
-      var origChildPath = origChild && origChild.getAttribute('l10n-path');
-      if (origChildPath) {
-        origChild = getElementByPath(origChildPath, origNode);
-      }
-      if ( ! origChild) {
-        continue;
-      }
+  var l10nId = node.getAttribute('l10n-id');
+  if (!l10nId)
+    return;
 
-      for (var k=0, origAttr; origAttr = origChild.attributes[k]; k++) {
-        // if ( ! origAttr.specified) continue;  // for IE?
-        if ( ! child.hasAttribute(origAttr.name)) {
-          child.setAttribute(origAttr.nodeName, origAttr.value);
-        }
+  var args;
+
+  // node.nodeData must not be exposed
+  if (node.nodeData) {
+    args = node.nodeData;
+  } else if (node.hasAttribute('l10n-args')) {
+    args = JSON.parse(node.getAttribute('l10n-args'));
+    node.nodeData = args;
+  }
+  // get attributes from the LO
+  var attrs = ctx.getAttributes(l10nId, args);
+  if (attrs) {
+    for (var j in attrs) {
+      node.setAttribute(j, attrs[j]);
+    }
+  }
+  var valueFromCtx = ctx.get(l10nId, args);
+  if (valueFromCtx === null)
+    return;
+
+  // deep-copy the original node
+  var origNode = node.cloneNode(true);
+  node.innerHTML = valueFromCtx;
+
+  // overlay the attributes of descendant nodes
+  var children = node.getElementsByTagName('*');
+  for (var j = 0, child; child = children[j]; j++) {
+    // Match the child node with the equivalent node in origNode.
+    // The tricky part is that the node in origNode might have been 
+    // translated before; if so and if it had been reordered via 
+    // `l10n-path`, it's likely that even though the path points to it, it 
+    // actually _isn't_ the right node.
+    var origChild;
+    var path = child.getAttribute('l10n-path');
+    if (!path) {
+      path = getPathTo(child, node);
+    }
+    origChild = getElementByPath(path, origNode);
+    // If the origChild already has `l10n-path`, it's likely that it has
+    // already been translated once.  Follow its `l10n-path` again to find 
+    // the true source equivalent in origNode.
+    var origChildPath = origChild && origChild.getAttribute('l10n-path');
+    if (origChildPath) {
+      origChild = getElementByPath(origChildPath, origNode);
+    }
+    if (!origChild) {
+      continue;
+    }
+
+    for (var k = 0, origAttr; origAttr = origChild.attributes[k]; k++) {
+      if (!child.hasAttribute(origAttr.name)) {
+        child.setAttribute(origAttr.nodeName, origAttr.value);
       }
     }
   }
