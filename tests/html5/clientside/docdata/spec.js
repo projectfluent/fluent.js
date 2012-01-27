39be010bec3feed637f3b427a67a2f7a3b201a62
Stas Malolepszy <stas@mozilla.com>
2012-01-27 14:52:25 +0100

    rewrite docdata

--- tests/html5/clientside/docdata/spec.js
+++ tests/html5/clientside/docdata/spec.js
@@ -1,11 +1,38 @@
-var utests = {
-  'retranslate': function(nodes) {
+describe("localization", function() {
+  it("title should be 'Downloading 5 files'", function() {
+    var node = document.querySelector('[l10n-id="window_title"]');
+    expect(node.textContent).toEqual('Downloading 5 files');
+  });
+  it("input label should be 'Name'", function() {
+    var node = document.querySelector('[l10n-id="name"]');
+    expect(node.textContent).toEqual('Name');
+  });
+  it("input value should be 'Maggie'", function() {
+    var node = document.querySelector('[l10n-id="name_input"]');
+    expect(node.getAttribute('value')).toEqual('Maggie');
+  });
+  it("download status should be 'Mark is currently downloading 5 files.'", function() {
+    var node = document.querySelector('[l10n-id="download_status"]');
+    expect(node.textContent).toEqual('Mark is currently downloading 5 files.');
+  });
+  it("mood message should be 'She's happy!'", function() {
+    var node = document.querySelector('[l10n-id="mood"]');
+    expect(node.textContent).toEqual('She\'s happy!');
+  });
+});
+
+describe("retranslation", function() {
+  beforeEach(function() {
     var button = document.getElementsByTagName('button')[0];
     button.click();
-    var mood = document.querySelectorAll('[l10n-id="mood"]')[0];
-    nodes.push(mood);
-    assertEquals(mood.textContent, "He's happy!");
+  });
+  afterEach(function() {
+    var mood = document.querySelector('[l10n-id="mood"]');
     mood.innerHTML = "She's happy!";
-  },
-}
+  });
 
+  it("mood message should be 'He's happy!'", function() {
+    var mood = document.querySelector('[l10n-id="mood"]');
+    expect(mood.textContent).toEqual('He\'s happy!');
+  });
+});
