87b7cea26214d1de0ca41ba48037f13c143851fa
Stas Malolepszy <stas@mozilla.com>
2012-01-27 13:04:01 +0100

    Rewrite attrscontrol

--- tests/html5/clientside/attrscontrol/spec.js
+++ tests/html5/clientside/attrscontrol/spec.js
@@ -1,9 +1,14 @@
-var utests = {
-  'name': function(nodes) {
-    var name = document.querySelectorAll('[l10n-id="test"]')[0];
-    nodes.push(name);
-    assertEquals(name.getAttribute('value'), 'passed');
-    assertEquals(name.getAttribute('title'), 'color should be green');
-    assertEquals(name.getAttribute('style'), 'color:green');
-  },
-}
+describe("Localization", function() {
+    it("value should be 'passed'", function() {
+        var node = document.querySelector('[l10n-id="test"]');
+        expect(node.getAttribute('value')).toEqual('passed');
+    });
+    it("title should be 'color should be green'", function() {
+        var node = document.querySelector('[l10n-id="test"]');
+        expect(node.getAttribute('title')).toEqual('color should be green');
+    });
+    it("style should be 'color:green'", function() {
+        var node = document.querySelector('[l10n-id="test"]');
+        expect(node.getAttribute('style')).toEqual('color:green');
+    });
+});
