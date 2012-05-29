f8c5eeb38a2d801979c8d5c03bf573b92e745050
Zbigniew Braniecki <gandalf@mozilla.com>
2012-05-29 17:45:14 +0200

    more work on update-locale tool

--- python/tools/env/update-locale.py
+++ python/tools/env/update-locale.py
@@ -2,6 +2,7 @@ import os
 import codecs
 
 import l20n.format.lol.parser as parser
+import l20n.format.lol.serializer as serializer
 import l20n.format.lol.ast as ast
 
 def read_file(path):
@@ -23,6 +24,18 @@ def reduce_complex_string(s):
         return s
     raise Exception("FUCK!")
 
+def add_entity(lol, k, value):
+    id = ast.Identifier(k)
+    entity = ast.Entity(id)
+    entity.value = value
+    lol.body.append(entity)
+
+def remove_entity(lol, id):
+    for n,elem in enumerate(lol.body):
+        if isinstance(elem, ast.Entity):
+            if elem.id.name == id:
+                del lol.body[n]
+
 def diff_entity(source, orig, trans):
     sval = reduce_complex_string(source)
     oval = reduce_complex_string(orig)
@@ -30,12 +43,12 @@ def diff_entity(source, orig, trans):
 
     if oval is None:
         return 'added'
+    if trans is None:
+        return 'nottranslated'
     if sval is None:
         return 'removed'
     if not sval == oval:
         return 'outdated'
-    if trans is None:
-        return 'nottranslated'
     return 'uptodate'
 
 def get_entity_dict(lol):
@@ -57,7 +70,16 @@ def update_locale():
     trans_file = read_file(os.path.join('data', locale, '%s.lol' % module))
     source_file = read_file(os.path.join(mpath, 'locale', '%s.lol' % source_locale))
 
+    result = {
+        'nottranslated': {},
+        'outdated': {},
+        'obsolete': {},
+        'added': {},
+        'uptodate': {},
+    }
+
     p = parser.Parser()
+    s = serializer.Serializer()
 
     orig_lol = p.parse(orig_file)
     trans_lol = p.parse(trans_file)
@@ -67,24 +89,26 @@ def update_locale():
     trans_dict = get_entity_dict(trans_lol)
     source_dict = get_entity_dict(source_lol)
 
-    for k,entity in orig_dict.items():
-        res = diff_entity(source_dict.get(k, None),
-                          orig_dict[k],
-                          trans_dict.get(k, None))
-        print('%s - %s' % (k, res))
-        if res == 'added':
-            print('added')
-        elif res == 'removed':
-            print('removed')
-        elif res == 'outdated':
-            print('outdated')
-        continue
     for k,entity in source_dict.items():
-        if k not in orig_dict:
-            diff_entity(source_dict[k],
-                        None,
-                        None)
+        res = diff_entity(source_dict[k],
+                          orig_dict.get(k, None),
+                          trans_dict.get(k, None))
+        if res == 'added':
+            add_entity(orig_lol, k, entity)
+            add_entity(trans_lol, k, entity)
+        elif res == 'nottranslated':
+            pass
 
+    for k,entity in orig_dict.items():
+        if k not in source_dict:
+            remove_entity(orig_lol, k)
+            remove_entity(trans_lol, k)
+    new_trans_lol = s.serialize(trans_lol)
+    new_orig_lol = s.serialize(orig_lol)
+
+    write_file(os.path.join('data', locale, '%s.lol.orig2' % module), new_orig_lol)
+    write_file(os.path.join('data', locale, '%s.lol2' % module), new_trans_lol)
+    #print_result('homescreen', result)
 
 if __name__ == '__main__':
     update_locale()
