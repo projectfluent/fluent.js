ce41dd0e21e0667db511288dbdcdbc11c8026daf
Zbigniew Braniecki <gandalf@mozilla.com>
2012-08-10 04:13:14 -0700

    update l20n.js to properly preprocess AST

--- tests/lib/data/example1.lol
+++ tests/lib/data/example1.lol
@@ -1,1 +1,4 @@
+
 <foo "value">
+import('http://127.0.0.1/l20n/l20n.js/tests/lib/data/example3.lol')
+<foo4 "value4">
--- tests/lib/test.js
+++ tests/lib/test.js
@@ -9,14 +9,16 @@ var ctx = L20n.getContext();
 
 ctx.addResource('/l20n/l20n.js/tests/lib/data/example1.lol')
 
-//ctx.__addResourceAST('path2')
+ctx.addResource('/l20n/l20n.js/tests/lib/data/example2.lol')
 
 //ctx.__addResourceAST('path1')
 
 ctx.onReady = function() {
   console.log('-- ready!')
   console.log(ctx.getAST());
-  //console.log(ctx.get('foo'))
+  console.log(ctx.get('foo'))
+  console.log(ctx.get('foo2'))
+  console.log(ctx.get('foo3'))
 }
 
 //console.log('01 Firefox', obj['brandName1'].get(obj));
