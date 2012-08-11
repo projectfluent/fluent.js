45817d6a1fba0c3560468a026446219676646a1a
Stas Malolepszy <stas@mozilla.com>
2012-08-10 17:29:07 -0700

    Fixtures for tests are now in tests/fixtures/json
    The basic*.lol files have been consolidated into simple_values.lol.
    The examples use more tangible strings, like "Firefox" and "Aurora".

--- tests/compiler/simple_values.js
+++ tests/compiler/simple_values.js
@@ -4,12 +4,18 @@ var Compiler = process.env.L20N_COV
   : require('../../lib/compiler.js');
 
 function read(filename) {
-  return JSON.parse(fs.readFileSync(filename)).body;
+  var fixtures = './tests/fixtures/json/';
+  return JSON.parse(fs.readFileSync(fixtures + filename)).body;
 }
 
 describe('Basic entities', function(){
+  var filename = 'simple_values.json';
   var ast;
   var obj;
+
+  before(function() {
+    ast = read(filename);
+  });
 
   beforeEach(function() {
     obj = {};
@@ -17,86 +23,71 @@ describe('Basic entities', function(){
   });
 
   describe('Simple value', function(){
-    var filename = './tests/fixtures/basic1.json';
-    before(function() { 
-      ast = read(filename);
-    });
-
     describe('Simple string value', function(){
-      it('is "Simple"', function(){
-        var value = obj['basic1'].get();
-        value.should.equal("Basic 1");
+      it('is "Firefox"', function(){
+        var value = obj['brandName'].get();
+        value.should.equal("Firefox");
       });
     });
   });
 
   describe('Simple array', function(){
-    var filename = './tests/fixtures/basic2.json';
-    before(function() { 
-      ast = read(filename);
-    });
-
     describe('an array without an index', function(){
-      it('is "One" when called without an index', function(){
-        var value = obj['basic21'].get(obj);
-        value.should.equal('One');
+      it('is "Firefox" when called without an index', function(){
+        var value = obj['brandName11'].get(obj);
+        value.should.equal('Firefox');
       });
-      it('is "Two" when called with [1]', function(){
-        var value = obj['basic21'].get(obj, {}, [1]);
-        value.should.equal('Two');
+      it('is "Aurora" when called with [1]', function(){
+        var value = obj['brandName11'].get(obj, {}, [1]);
+        value.should.equal('Aurora');
       });
     });
     describe('an array with an index of [1]', function(){
-      it('is "Two" when called without an index', function(){
-        var value = obj['basic22'].get(obj);
-        value.should.equal('Two');
+      it('is "Firefox" when called without an index', function(){
+        var value = obj['brandName12'].get(obj);
+        value.should.equal('Firefox');
       });
-      it('is "One" when called with [0]', function(){
-        var value = obj['basic22'].get(obj, {}, [0]);
-        value.should.equal('One');
+      it('is "Firefox" when called with [0]', function(){
+        var value = obj['brandName12'].get(obj, {}, [0]);
+        value.should.equal('Firefox');
       });
     });
   });
 
   describe('Simple hash', function(){
-    var filename = './tests/fixtures/basic3.json';
-    before(function() { 
-      ast = read(filename);
-    });
-
     describe('a hash with no index and no default value', function(){
       it('is "Firefox"', function(){
-        var value = obj['brandName1'].get(obj);
+        var value = obj['brandName21'].get(obj);
         value.should.equal('Firefox');
       });
       it('is "Aurora when called with an index of ["feminine"] "', function(){
-        var value = obj['brandName1'].get(obj, {}, ['feminine']);
+        var value = obj['brandName21'].get(obj, {}, ['feminine']);
         value.should.equal('Aurora');
       });
     });
     describe('a hash with no index and with a default value', function(){
       it('is "Aurora"', function(){
-        var value = obj['brandName2'].get(obj);
+        var value = obj['brandName22'].get(obj);
         value.should.equal('Aurora');
       });
       it('is "Firefox" when called with an index of ["masculine"] ', function(){
-        var value = obj['brandName2'].get(obj, {}, ['masculine']);
+        var value = obj['brandName22'].get(obj, {}, ['masculine']);
         value.should.equal('Firefox');
       });
     });
     describe('a hash with an index and no default value', function(){
       it('is "Aurora"', function(){
-        var value = obj['brandName3'].get(obj);
+        var value = obj['brandName23'].get(obj);
         value.should.equal('Aurora');
       });
       it('is "Firefox" when called with an index of ["masculine"] ', function(){
-        var value = obj['brandName3'].get(obj, {}, ['masculine']);
+        var value = obj['brandName23'].get(obj, {}, ['masculine']);
         value.should.equal('Firefox');
       });
     });
     describe('a hash with too many index keys and no default value', function(){
       it('is "Aurora"', function(){
-        var value = obj['brandName3'].get(obj);
+        var value = obj['brandName23'].get(obj);
         value.should.equal('Aurora');
       });
     });
