f649cf0624fa3a90d7d0480450e71d148dd0fee9
Stas Malolepszy <stas@mozilla.com>
2012-08-17 03:40:17 +0200

    Change how variables are stored in locals (+tests)
    Macro arguments are no longer resolved in the callExpression.  This
    made it impossible to pass an entity or a hash to a macro, and resulted
    in weird behavior when passing a macro.
    
    Instead, argument expressions are now simply called once, so that the
    actual arguments passed to the macro are objects, not expressions, and
    they have their own locals stored in the macro's locals, too.
    
    For instance, in a callExpression like this one:
    
      plural(7)
    
    '7' is a stringLiteral expression, and calling it returns 7, which is
    then passed to the macro expression.
    
    Similarly, for a macro call like so:
    
      getDefaultValue(brandName)
    
    'brandName' is an indentifier expression, and calling it return the
    entity object called 'brandName.'
    
    Note that we must call the expression before passing it to the macro
    expression.  Otherwise, we would be passing expression functions around
    (e.g., propertyExpressions) instead of actual objects that they
    evaluate to, and then we'd call these expression functions with wrong
    locals.

--- tests/compiler/macros.js
+++ tests/compiler/macros.js
@@ -8,7 +8,7 @@ function read(filename) {
   return JSON.parse(fs.readFileSync(fixtures + filename)).body;
 }
 
-describe('Funky macros', function(){
+describe('Macros', function(){
   var filename = 'macros.json';
   var ast;
   var env = {
@@ -25,22 +25,174 @@ describe('Funky macros', function(){
     Compiler.compile(ast, env.entries);
   });
 
-  describe('nth Fibonnaci number', function(){
-    it('returns 6765 for [20]', function(){
-      var value = env.entries['callFib'].toString();
-      value.should.equal('6765');
+  describe('Simple macros', function(){
+    describe('Double', function(){
+      it('returns 6 for 3', function(){
+        var value = env.entries['callDouble'].toString();
+        value.should.equal('6');
+      });
+    });
+    describe('Zero', function(){
+      it('returns 0 for 1', function(){
+        var value = env.entries['callZero'].toString();
+        value.should.equal('0');
+      });
     });
   });
-  describe('factorial', function(){
-    it('returns 120 for [5]', function(){
-      var value = env.entries['callFac'].toString();
-      value.should.equal('120');
+
+  describe('Funky macros', function(){
+    describe('nth Fibonnaci number', function(){
+      it('returns 6765 for [20]', function(){
+        var value = env.entries['callFib'].toString();
+        value.should.equal('6765');
+      });
+    });
+    describe('factorial', function(){
+      it('returns 120 for [5]', function(){
+        var value = env.entries['callFac'].toString();
+        value.should.equal('120');
+      });
+    });
+    describe('plural', function(){
+      it('returns "many" for 0', function(){
+        var value = env.entries['callPlural0'].toString();
+        value.should.equal('many');
+      });
+      it('returns "one" for 1', function(){
+        var value = env.entries['callPlural1'].toString();
+        value.should.equal('one');
+      });
+      it('returns "few" for 2', function(){
+        var value = env.entries['callPlural2'].toString();
+        value.should.equal('few');
+      });
+      it('returns "many" for 5', function(){
+        var value = env.entries['callPlural5'].toString();
+        value.should.equal('many');
+      });
+      it('returns "many" for 11', function(){
+        var value = env.entries['callPlural11'].toString();
+        value.should.equal('many');
+      });
+      it('returns "few" for 22', function(){
+        var value = env.entries['callPlural22'].toString();
+        value.should.equal('few');
+      });
+      it('returns "many" for 101', function(){
+        var value = env.entries['callPlural101'].toString();
+        value.should.equal('many');
+      });
+      it('returns "few" for 102', function(){
+        var value = env.entries['callPlural102'].toString();
+        value.should.equal('few');
+      });
+      it('returns "many" for 111', function(){
+        var value = env.entries['callPlural111'].toString();
+        value.should.equal('many');
+      });
+      it('returns "many" for 121', function(){
+        var value = env.entries['callPlural121'].toString();
+        value.should.equal('many');
+      });
+      it('returns "few" for 122', function(){
+        var value = env.entries['callPlural122'].toString();
+        value.should.equal('few');
+      });
     });
   });
-  describe('logical OR expression', function(){
-    it('returns the second operand if the first one is false', function(){
-      var value = env.entries['callZeroOrFac'].toString();
-      value.should.equal('120');
+
+  describe('Nested macros', function(){
+    describe('Logical OR expression', function(){
+      it('returns the second operand if the first one is false', function(){
+        var value = env.entries['callZeroOrFac'].toString();
+        value.should.equal('120');
+      });
+    });
+    describe('Quad', function(){
+      it('returns 28 for 7', function(){
+        var value = env.entries['callQuad'].toString();
+        value.should.equal('28');
+      });
+    });
+  });
+
+  describe('Passing references to entires', function(){
+    describe('Calling a macro indirectly', function(){
+      it('returns 120 for fac and 5', function(){
+        var value = env.entries['callCall'].toString();
+        value.should.equal('120');
+      });
+      it('throws a TypeError for "fac" and 5', function(){
+        (function () {
+          var value = env.entries['callCallString'].toString();
+        }).should.throw(TypeError);
+      });
+    });
+    describe('Passing an entity to a macro', function(){
+      it('returns "Firefox"', function(){
+        var value = env.entries['callGet'].toString();
+        value.should.equal('Firefox');
+      });
+      it('returns "Firefox" with a property accessor in the complexString', function(){
+        var value = env.entries['callGet1'].toString();
+        value.should.equal('Firefox');
+      });
+      it('returns "Firefox\'s" with a property accessor in the complexString', function(){
+        var value = env.entries['callGet2'].toString();
+        value.should.equal('Firefox\'s');
+      });
+      it('returns "Firefox\'s" with a property accessor in the macro expression', function(){
+        var value = env.entries['callGetGenitive'].toString();
+        value.should.equal('Firefox\'s');
+      });
+    });
+    describe('Calling a macro which returns an entity', function(){
+      it('should resolve to "Firefox"', function(){
+        var value = env.entries['callGetBrandName'].toString();
+        value.should.equal('Firefox');
+      });
+    });
+    describe('Calling a macro which returns an entity\'s hashItem which is a string', function(){
+      it('should resolve to "Firefox\'s" when called with "genitive"', function(){
+        var value = env.entries['callGetBrandNameCase'].toString();
+        value.should.equal('Firefox\'s');
+      });
+    });
+    describe('', function(){
+      it('', function(){
+        var value = env.entries['callGetBrandNameLength1'].toString();
+        value.should.equal('Mozilla Firefox');
+      });
+      it('', function(){
+        var value = env.entries['callGetBrandNameLength2'].toString();
+        value.should.equal('Mozilla Firefox\'s');
+      });
+      it('', function(){
+        var value = env.entries['callGetBrandNameLengthGenitive'].toString();
+        value.should.equal('Mozilla Firefox\'s');
+      });
+    });
+    describe('', function(){
+      it('', function(){
+        var value = env.entries['callGetBrandNameThisLength1'].toString();
+        value.should.equal('Mozilla Firefox');
+      });
+      it('', function(){
+        var value = env.entries['callGetBrandNameThisLength2'].toString();
+        value.should.equal('Mozilla Firefox\'s');
+      });
+      it('', function(){
+        var value = env.entries['callGetBrandNameThisGenitive'].toString();
+        value.should.equal('Mozilla Firefox\'s');
+      });
+      it('', function(){
+        var value = env.entries['callGetBrandNameThis1'].toString();
+        value.should.equal('Mozilla Firefox');
+      });
+      it('', function(){
+        var value = env.entries['callGetBrandNameThis2'].toString();
+        value.should.equal('Mozilla Firefox\'s');
+      });
     });
   });
 });
