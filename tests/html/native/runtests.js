67cb11f3dec2814f3d18ade5075e01dfa3e5a04b
Stas Malolepszy <stas@mozilla.com>
2013-02-05 11:51:15 +0100

    remove old examples and tests

--- tests/html/native/runtests.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var jasmineEnv = jasmine.getEnv();
-    var reporter = new jasmine.HtmlReporter();
-    jasmineEnv.addReporter(reporter);
-    jasmineEnv.execute();
-});
