967cb55dd98cd3f5ec5f3664a54f241eaa4a7f7b
Zbigniew Braniecki <gandalf@mozilla.com>
2013-06-12 20:38:30 -0700

    more tests for parser

--- tests/lib/parser.js
+++ tests/lib/parser.js
@@ -121,4 +121,51 @@ describe('Example', function() {
     ast.body[0].attrs[2].key.name.should.equal('attr3');
     ast.body[0].attrs[2].value.content.should.equal('foo3');
   });
+  it('attributes with indexes', function() {
+    var ast = parser.parse("<id attr[2]: 'foo'>");
+    ast.body[0].attrs[0].index[0].value.should.equal(2);
+
+    ast = parser.parse("<id attr[2+3?'foo':'foo2']: 'foo'>");
+    ast.body[0].attrs[0].index[0].test.left.value.should.equal(2);
+    ast.body[0].attrs[0].index[0].test.right.value.should.equal(3);
+
+    ast = parser.parse("<id attr[2, 3]: 'foo'>");
+    ast.body[0].attrs[0].index[0].value.should.equal(2);
+    ast.body[0].attrs[0].index[1].value.should.equal(3);
+  });
+  it('atribute errors', function() {
+    var strings = [
+      '<id : "foo">',
+      "<id 2: >",
+      "<id a: >",
+      "<id: ''>",
+      "<id a: b:>",
+      "<id a: 'foo' 'heh'>",
+      "<id a: 2>",
+      "<id 'a': 'a'>",
+      "<id \"a\": 'a'>",
+      "<id 2: 'a'>",
+      "<id a2:'a'a3:'v'>", 
+    ];
+    for (var i in strings) {
+      var ast = parser.parse(strings[i]);
+      ast.body[0].type.should.equal('JunkEntry');
+    }
+  });
+  it('hash value', function() {
+    var ast = parser.parse("<id {}>");
+    ast.body.length.should.equal(1);
+    ast.body[0].value.content.length.should.equal(0);
+    
+    var ast = parser.parse("<id {a: 'b', a2: 'c', d: 'd' }>");
+    ast.body.length.should.equal(1);
+    ast.body[0].value.content.length.should.equal(3);
+    ast.body[0].value.content[0].value.content.should.equal('b');
+    
+    var ast = parser.parse("<id {a: '2', b: '3'} >");
+    ast.body.length.should.equal(1);
+    ast.body[0].value.content.length.should.equal(2);
+    ast.body[0].value.content[0].value.content.should.equal('2');
+    ast.body[0].value.content[1].value.content.should.equal('3');
+  });
 });
