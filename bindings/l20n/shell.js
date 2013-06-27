4210be148c3d9936c0b8e1386c91c3815292d5e9
Staś Małolepszy <stas@mozilla.com>
2013-06-27 15:32:32 -0700

    Bug 886824 - Introduce shell target for l20n.js; r=gandalf

---
+++ bindings/l20n/shell.js
@@ -0,0 +1,9 @@
+define(function (require, exports, module) {
+  'use strict';
+
+  var L20n = require('../l20n');
+  L20n.RetranslationManager = require('./retranslation').RetranslationManager;
+
+  return L20n;
+
+});
