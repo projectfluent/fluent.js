4d8f813499068f10f1d0bf92eeb61a54966222ec
Zbigniew Braniecki <gandalf@mozilla.com>
2013-08-23 14:07:46 -0700

    switch from window to this

--- build/suffix/gaia.js
+++ build/suffix/gaia.js
@@ -1,8 +1,8 @@
 // attach the L20n singleton to the global object
-window.L20n = require('l20n');
+this.L20n = require('l20n');
 
 // hook up the HTML bindings
 require('l20n/gaia');
 
 // close the function defined in build/prefix/microrequire.js
-})(window);
+})(this);
