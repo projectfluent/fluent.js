366d7dc2ab6648b1a00be6b567370139321be2d2
Staś Małolepszy <stas@mozilla.com>
2013-08-29 00:48:57 +0200

    remove other bindings

--- bindings/l20n/shell.js
+++ bindings/l20n/shell.js
@@ -2,7 +2,6 @@ define(function (require, exports, module) {
   'use strict';
 
   var L20n = require('../l20n');
-  L20n.RetranslationManager = require('./retranslation').RetranslationManager;
 
   return L20n;
 
