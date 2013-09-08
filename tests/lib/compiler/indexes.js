a42533b089f5f83291b336afca6e861e08fae9a5
Staś Małolepszy <stas@mozilla.com>
2013-09-08 14:00:43 +0200

    compiler tests: indexes

--- tests/lib/compiler/indexes.js
+++ tests/lib/compiler/indexes.js
@@ -10,182 +10,45 @@ describe('Index', function(){
   var source, ctxdata, ast, env;
   beforeEach(function() {
     ast = parser.parse(source);
+    ast.body['plural'] = {
+      type: 'Macro',
+      args: [{
+        type: 'Identifier',
+        name: 'n'
+      }],
+      expression: function(n) {
+        return (n == 1) ? 'one' : 'other';
+      }
+    };
     env = compiler.compile(ast);
   });
 
-  describe('IndexError in an entity in the index', function(){
+  describe('Cyclic reference to the same entity', function(){
     before(function() {
-      source = '                                                              \
-        <bar[1 - "a"] {                                                       \
-         *key: "Bar"                                                          \
-        }>                                                                    \
-        <foo[bar] {                                                           \
-         *key: "Foo"                                                          \
-        }>                                                                    \
-      ';
+      source = [
+        'foo={[ plural(foo) ]}',
+        'foo[one]=One'
+      ].join('\n');
     });
     it('throws', function() {
       (function() {
         env.foo.getString();
-      }).should.throw(/takes two numbers/);
+      }).should.throw(/must be numbers/);
     });
   });
 
-  describe('Cyclic reference to named entity', function(){
+  describe('Reference from an attribute to the value of the same entity', function(){
     before(function() {
-      source = '                                                              \
-        <foo[foo] {                                                           \
-         *key: "Foo"                                                          \
-        }>                                                                    \
-      ';
+      source = [
+        'foo=Foo',
+        'foo.attr={[ plural(foo) ]}',
+        'foo.attr[one]=One'
+      ].join('\n');
     });
     it('throws', function() {
       (function() {
-        env.foo.getString();
-      }).should.throw(/Cyclic reference detected/);
-    });
-  });
-
-  describe('Reference to an existing member of named entity', function(){
-    before(function() {
-      source = '                                                              \
-        <foo[foo.bar] {                                                       \
-         *key: "Foo",                                                         \
-          bar: "key"                                                          \
-        }>                                                                    \
-      ';
-    });
-    it('returns the value', function() {
-      env.foo.getString().should.equal('Foo');
-    });
-  });
-
-  describe('Cyclic reference to a missing member of named entity', function(){
-    before(function() {
-      source = '                                                              \
-        <foo[foo.xxx] {                                                       \
-         *key: "Foo",                                                         \
-          bar: "key"                                                          \
-        }>                                                                    \
-      ';
-    });
-    it('throws', function() {
-      (function() {
-        env.foo.getString();
-      }).should.throw(/Cyclic reference detected/);
-    });
-  });
-
-  describe('Reference to an existing attribute of named entity', function(){
-    before(function() {
-      source = '                                                              \
-        <foo[foo::attr] {                                                     \
-         *key: "Foo",                                                         \
-          bar: "key"                                                          \
-        }                                                                     \
-         attr: "key"                                                          \
-        >                                                                     \
-      ';
-    });
-    it('returns the value', function() {
-      env.foo.getString().should.equal('Foo');
-    });
-  });
-
-  describe('Reference to a missing attribute of named entity', function(){
-    before(function() {
-      source = '                                                              \
-        <foo[foo::missing] {                                                  \
-         *key: "Foo",                                                         \
-          bar: "key"                                                          \
-        }                                                                     \
-         attr: "key"                                                          \
-        >                                                                     \
-      ';
-    });
-    it('throws', function() {
-      (function() {
-        env.foo.getString();
-      }).should.throw(/has no attribute/);
-    });
-  });
-
-  describe('Cyclic reference to this entity', function(){
-    before(function() {
-      source = '                                                              \
-        <foo[~] {                                                             \
-         *key: "Foo"                                                          \
-        }>                                                                    \
-      ';
-    });
-    it('throws', function() {
-      (function() {
-        env.foo.getString();
-      }).should.throw(/Cyclic reference detected/);
-    });
-  });
-
-  describe('Reference to an existing member of this entity', function(){
-    before(function() {
-      source = '                                                              \
-        <foo[~.bar]   {                                                       \
-         *key: "Foo",                                                         \
-          bar: "key"                                                          \
-        }>                                                                    \
-      ';
-    });
-    it('returns the value', function() {
-      env.foo.getString().should.equal('Foo');
-    });
-  });
-
-  describe('Cyclic reference to a missing member of this entity', function(){
-    before(function() {
-      source = '                                                              \
-        <foo[~.xxx] {                                                         \
-         *key: "Foo",                                                         \
-          bar: "key"                                                          \
-        }>                                                                    \
-      ';
-    });
-    it('throws', function() {
-      (function() {
-        env.foo.getString();
-      }).should.throw(/Cyclic reference detected/);
-    });
-  });
-
-  describe('Reference to an existing attribute of this entity', function(){
-    before(function() {
-      source = '                                                              \
-        <foo[~::attr] {                                                       \
-         *key: "Foo",                                                         \
-          bar: "key"                                                          \
-        }                                                                     \
-         attr: "key"                                                          \
-        >                                                                     \
-      ';
-    });
-    it('returns the value', function() {
-      env.foo.getString().should.equal('Foo');
-    });
-  });
-
-  describe('Reference to a missing attribute of this entity', function(){
-    before(function() {
-      source = '                                                              \
-        <foo[~::missing] {                                                    \
-         *key: "Foo",                                                         \
-          bar: "key"                                                          \
-        }                                                                     \
-         attr: "key"                                                          \
-        >                                                                     \
-      ';
-    });
-    it('throws', function() {
-      (function() {
-        env.foo.getString();
-      }).should.throw(/has no attribute/);
+        env.foo.get();
+      }).should.throw(/must be numbers/);
     });
   });
 
