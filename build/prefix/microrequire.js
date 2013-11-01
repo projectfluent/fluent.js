cef912a186689698c632c39f7335ac4f5d2d5de8
Zbigniew Braniecki <gandalf@mozilla.com>
2013-11-01 16:06:11 -0700

    add vim header per Vivien's feedback

--- build/prefix/microrequire.js
+++ build/prefix/microrequire.js
@@ -1,3 +1,6 @@
+/* -*- Mode: js; js-indent-level: 2; indent-tabs-mode: nil -*- */
+/* vim: set shiftwidth=2 tabstop=2 autoindent cindent expandtab: */
+
 (function(window, undefined) {
 
 function define(name, payload) {
