986794bcd653b5b3c18a1a629852ec4c15612f22
Staś Małolepszy <stas@mozilla.com>
2013-12-09 12:35:39 -0700

    Bug 942183 - Error when localizeNode is done quickly after requestLocales. r=gandalf

--- tests/lib/context/frozen.js
+++ tests/lib/context/frozen.js
@@ -2,6 +2,14 @@ var Context = process.env.L20N_COV ?
   require('../../../build/cov/lib/l20n/context').Context :
   require('../../../lib/l20n/context').Context;
 
+function whenReady(ctx, callback) {
+  'use strict';
+  ctx.addEventListener('ready', function onReady() {
+    ctx.removeEventListener('ready', onReady);
+    callback();
+  });
+}
+
 describe('A non-frozen context', function() {
   'use strict';
 
@@ -163,5 +171,37 @@ describe('A frozen, ready context', function() {
     (function(){
       ctx.requestLocales('en-US');
     }).should.not.throw();
+  });
+});
+
+describe('A frozen, ready context', function() {
+  'use strict';
+
+  // jsHint incorrectly claims function expressions on which the property
+  // is accessed just after its definition doesn't require parens;
+  // ignore this warning.
+  /* jshint -W068 */
+
+  var ctx;
+  beforeEach(function(done) {
+    ctx = new Context();
+    ctx.linkResource(function(locale) {
+      return __dirname + '/fixtures/' + locale + '.l20n';
+    });
+    whenReady(ctx, done);
+    ctx.registerLocales('en-US', ['de', 'en-US', 'pl']);
+    ctx.requestLocales('en-US');
+  });
+
+  it('should return translations for the current built fallback chain ', function() {
+    // Bug 942183 - Error when localizeNode is done quickly after 
+    // requestLocales https://bugzil.la/942183
+    // Changing locales triggers the build process for the 'pl' locale.  
+    // However, synchronous methods called right after the change should still 
+    // return translations for the previous fallback chain
+    ctx.requestLocales('pl');
+    var entity = ctx.getEntitySync('foo');
+    entity.value.should.equal('Foo en-US');
+    entity.locale.should.equal('en-US');
   });
 });
