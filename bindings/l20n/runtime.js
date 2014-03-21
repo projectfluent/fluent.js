b67c613521e5d6a452b47fa990184b4944e86597
Zbigniew Braniecki <gandalf@mozilla.com>
2014-03-20 17:02:38 -0700

    initial rewrite of the runtime bindings

--- bindings/l20n/runtime.js
+++ bindings/l20n/runtime.js
@@ -1,398 +1,208 @@
-  'use strict';
+'use strict';
 
-  var Context = require('./context').Context;
-  var io = require('./platform/io');
+var Context = require('./context').Context;
 
-  var isBootstrapped = false;
-  var isPretranslated = false;
+var isPretranslated = false;
+var rtlList = ['ar', 'he', 'fa', 'ps', 'ur'];
 
-  var ctx = new Context();
-  navigator.mozL10n = createPublicAPI(ctx);
-  ctx.addEventListener('error', logMessage.bind(null, 'error'));
-  ctx.addEventListener('warning', logMessage.bind(null, 'warn'));
+var ctx = new Context();
 
-  isPretranslated = document.documentElement.lang === navigator.language;
+navigator.mozL10n = {
+  translate: translateFragment,
+  localize: localizeElement,
+  get: ctx.get.bind(ctx),
+  ready: ctx.ready.bind(ctx),
+  get readyState() {
+    return ctx.isReady ? 'complete' : 'loading';
+  },
+  language: {
+    set code(lang) {
+      initLocale(lang, true);
+    },
+    get code() {
+      return ctx.supportedLocales[0];
+    },
+    get direction() {
+      return getDirection(ctx.supportedLocales[0]);
+    }
+  }
+};
+
+function getDirection(lang) {
+  return (rtlList.indexOf(lang) >= 0) ? 'rtl' : 'ltr';
+}
+
+var readyStates = {
+  'loading': 0,
+  'interactive': 1,
+  'complete': 2
+};
+
+function waitFor(state, callback) {
+  state = readyStates[state];
+  if (readyStates[document.readyState] >= state) {
+    callback();
+    return;
+  }
+  document.addEventListener('readystatechange', function l10n_onrsc() {
+    if (readyStates[document.readyState] >= state) {
+      document.removeEventListener('readystatechange', l10n_onrsc);
+      callback();
+    }
+  });
+}
+
+if (window.document) {
+  isPretranslated = (document.documentElement.lang === navigator.language);
 
   if (isPretranslated) {
-    // if the DOM has been pretranslated, defer bootstrap as long as possible
     waitFor('complete', function() {
-      window.setTimeout(bootstrap);
+      window.setTimeout(initDocumentLocalization.bind(null, initLocale));
     });
   } else {
-    // otherwise, if the DOM is loaded, bootstrap now to fire 'localized'
-    if (document.readyState === 'complete') {
-      window.setTimeout(bootstrap);
+    initDocumentLocalization(initLocale);
+  }
+}
+
+function initDocumentLocalization(cb) {
+  var resLinks = document.head.querySelectorAll('link[type="application/l10n"]');
+  var iniLinks = [];
+  var link;
+
+  for (link of resLinks) {
+    var url = link.getAttribute('href');
+    var type = url.substr(url.lastIndexOf('.') + 1);
+    if (type === 'ini') {
+      iniLinks.push(url);
     } else {
-      // or wait for the DOM to be interactive to try to pretranslate it 
-      // using the inline resources
-      waitFor('interactive', pretranslate);
+      ctx.resLinks.push(url);
     }
   }
 
-  function waitFor(state, callback) {
-    if (document.readyState === state) {
-      callback();
-      return;
-    }
-    document.addEventListener('readystatechange', function l10n_onrsc() {
-      if (document.readyState === state) {
-        callback();
-      }
-    });
+  ctx.ready(fireLocalizedEvent);
+
+  var iniLoads = iniLinks.length;
+  if (iniLoads === 0) {
+    onIniLoaded();
   }
 
-  function pretranslate() {
-    if (inlineLocalization()) {
-      // if inlineLocalization succeeded, defer bootstrap
-      waitFor('complete', function() {
-        window.setTimeout(bootstrap);
-      });
-    } else {
-      bootstrap();
-    }
-  }
-
-  function inlineLocalization() {
-    var body = document.body;
-    var script = body.querySelector('script[type="application/l10n"][lang="' +
-                                    navigator.language + '"]');
-    if (!script) {
-      return false;
-    }
-
-    ctx.supportedLocales = [navigator.language];
-    var locale = ctx.getLocale(navigator.language);
-    // the inline localization is happenning very early, when the ctx is not
-    // yet ready and when the resources haven't been downloaded yet;  add the
-    // inlined JSON directly to the current locale
-    locale.addAST(JSON.parse(script.innerHTML));
-    // avoid errors in ctx.get*
-    ctx.isReady = true;
-    // make sure the locale is not actually built in ctx.get*
-    locale.isReady = true;
-    // localize the visible DOM
-    translateFragment(ctx);
-    // record the fact that the locale already has the inlined strings
-    locale.isPartial = true;
-    // restore readiness of locale and ctx thanks to which proper localization
-    // resources can be added later on (in ctx.freeze)
-    locale.isReady = false;
-    ctx.isReady = false;
-    // the visible DOM is now pretranslated
-    isPretranslated = true;
-    return true;
-  }
-
-  function bootstrap(forcedLocale) {
-    isBootstrapped = true;
-
-    var availableLocales = [];
-
-    var head = document.head;
-    var resLinks = head.querySelectorAll('link[type="application/l10n"]');
-    var iniLinks = [];
-    var i;
-
-    for (i = 0; i < resLinks.length; i++) {
-      var url = resLinks[i].getAttribute('href');
-      var type = url.substr(url.lastIndexOf('.') + 1);
-      if (type === 'ini') {
-        iniLinks.push(url);
-      } else {
-        ctx.resLinks.push(url);
-      }
-    }
-
-    ctx.ready(function() {
-      // XXX instead of using a flag, we could store the list of 
-      // yet-to-localize nodes that we get from the inline context, and 
-      // localize them here.
-      if (!isPretranslated) {
-        translateFragment(ctx);
-      }
-      isPretranslated = false;
-      fireLocalizedEvent(ctx);
-    });
-
-    var iniLoads = iniLinks.length;
-
-    if (iniLoads === 0) {
-      onIniLoaded();
-    }
-    function onIniLoaded() {
-      iniLoads--;
-      if (iniLoads <= 0) {
-        ctx.registerLocales('en-US', getAvailable());
-        ctx.requestLocales(forcedLocale || navigator.language);
-      }
-    }
-
-    for (i = 0; i < iniLinks.length; i++) {
-      loadINI(iniLinks[i], onIniLoaded);
-    }
-
-    // listen to language change events
-    if ('mozSettings' in navigator && navigator.mozSettings) {
-      navigator.mozSettings.addObserver('language.current', function(event) {
-        ctx.requestLocales(event.settingValue);
-      });
-    }
-  }
-
-  function loadINI(url, cb) {
-    io.load(url, function(err, source) {
-      if (!source) {
-        cb();
-        return;
-      }
-
-      var ini = parseINI(source, url);
-      var pos = ctx.resLinks.indexOf(url);
-
-      var patterns = [];
-      for (var i = 0; i < ini.resources.length; i++) {
-        patterns.push(ini.resources[i].replace('en-US', '{{locale}}'));
-      }
-      var args = [pos, 1].concat(patterns);
-      ctx.resLinks.splice.apply(ctx.resLinks, args);
+  function onIniLoaded() {
+    iniLoads--;
+    if (iniLoads <= 0) {
       cb();
+    }
+  }
+
+  for (link of iniLinks) {
+    loadINI(link, onIniLoaded);
+  }
+}
+
+
+function loadINI(url, cb) {
+  io.load(url, function(err, source) {
+    if (!source) {
+      cb();
+      return;
+    }
+
+    var ini = parseINI(source, url);
+    var pos = ctx.resLinks.indexOf(url);
+
+    var patterns = ini.resources.map(function(x) {
+      return x.replace('en-US', '{{locale}}');
     });
+    var args = [pos, 1].concat(patterns);
+    ctx.resLinks.splice.apply(ctx.resLinks, args);
+    cb();
+  });
+}
+
+function relativePath(baseUrl, url) {
+  if (url[0] === '/') {
+    return url;
   }
 
-  function getAvailable() {
-    var metaLocs = document.head.querySelector('meta[name="locales"]');
-    if (metaLocs) {
-      return metaLocs.getAttribute('content').split(',').map(String.trim);
-    } else {
-      return [];
+  var dirs = baseUrl.split('/')
+    .slice(0, -1)
+    .concat(url.split('/'))
+    .filter(function(path) {
+      return path !== '.';
+    });
+
+  return dirs.join('/');
+}
+
+var iniPatterns = {
+  section: /^\s*\[(.*)\]\s*$/,
+  import: /^\s*@import\s+url\((.*)\)\s*$/i,
+  entry: /[\r\n]+/
+};
+
+function parseINI(source, iniPath) {
+  var entries = source.split(iniPatterns.entry);
+  var locales = ['en-US'];
+  var genericSection = true;
+  var uris = [];
+  var match;
+
+  for (var line of entries) {
+    // we only care about en-US resources
+    if (genericSection && iniPatterns['import'].test(line)) {
+      match = iniPatterns['import'].exec(line);
+      var uri = relativePath(iniPath, match[1]);
+      uris.push(uri);
+      continue;
+    }
+
+    // but we need the list of all locales in the ini, too
+    if (iniPatterns.section.test(line)) {
+      genericSection = false;
+      match = iniPatterns.section.exec(line);
+      locales.push(match[1]);
     }
   }
-
-  var patterns = {
-    section: /^\s*\[(.*)\]\s*$/,
-    import: /^\s*@import\s+url\((.*)\)\s*$/i,
-    entry: /[\r\n]+/
+  return {
+    locales: locales,
+    resources: uris
   };
+}
 
-  function parseINI(source, iniPath) {
-    var entries = source.split(patterns['entry']);
-    var locales = ['en-US'];
-    var genericSection = true;
-    var uris = [];
+function initLocale(lang, forced) {
+  ctx.registerLocales('en-US');
+  ctx.requestLocales(navigator.language);
+}
 
-    for (var i = 0; i < entries.length; i++) {
-      var line = entries[i];
-      // we only care about en-US resources
-      if (genericSection && patterns['import'].test(line)) {
-        var match = patterns['import'].exec(line);
-        var uri = relativePath(iniPath, match[1]);
-        uris.push(uri);
-        continue;
-      }
+function fireLocalizedEvent() {
+  var event = document.createEvent('Event');
+  event.initEvent('localized', false, false);
+  event.language = ctx.supportedLocales[0];
+  window.dispatchEvent(event);
+}
 
-      // but we need the list of all locales in the ini, too
-      if (patterns['section'].test(line)) {
-        genericSection = false;
-        var match = patterns['section'].exec(line);
-        locales.push(match[1]);
-      }
-    }
-    return {
-      locales: locales,
-      resources: uris
-    };
+function translateFragment(element) {
+  element = element || document.documentElement;
+
+  translateElement(element);
+  var nodes = getTranslatableChildren(element);
+  for (var node of nodes) {
+    translateElement(node);
   }
+}
 
-  function relativePath(baseUrl, url) {
-    if (url[0] == '/') {
-      return url;
-    }
+function getTranslatableChildren(element) {
+  return element ? element.querySelectorAll('*[data-l10n-id]') : [];
+}
 
-    var dirs = baseUrl.split('/')
-      .slice(0, -1)
-      .concat(url.split('/'))
-      .filter(function(path) {
-        return path !== '.';
-      });
-
-    return dirs.join('/');
+function localizeElement(element, id, args) {
+  if (!id) {
+    element.removeAttribute('data-l10n-id');
+    element.removeAttribute('data-l10n-args');
+    element.textContent = '';
+  } else {
+    element.textContent = '#'+id;
   }
+}
 
-  function createPublicAPI(ctx) {
-    var rtlLocales = ['ar', 'fa', 'he', 'ps', 'ur'];
-    return {
-      get: function l10n_get(id, data) {
-        var value = ctx.get(id, data);
-        if (value === null) {
-          return '';
-        }
-        return value;
-      },
-      localize: localizeNode.bind(null, ctx),
-      translate: translateFragment.bind(null, ctx),
-      language: {
-        get code() {
-          return ctx.supportedLocales[0];
-        },
-        set code(lang) {
-          ctx.requestLocales(lang);
-        },
-        get direction() {
-          if (rtlLocales.indexOf(ctx.supportedLocales[0]) >= 0) {
-            return 'rtl';
-          } else {
-            return 'ltr';
-          }
-        }
-      },
-      ready: ctx.ready.bind(ctx),
-      get readyState() {
-        return ctx.isReady ? 'complete' : 'loading';
-      }
-    };
-  }
-
-  var DEBUG = false;
-  function logMessage(type, e) {
-    if (DEBUG) {
-      console[type](e);
-    }
-  }
-
-  function getTranslatableChildren(element) {
-    return element ? element.querySelectorAll('*[data-l10n-id]') : [];
-  }
-
-
-  function getL10nAttributes(element) {
-    if (!element) {
-      return {};
-    }
-
-    var l10nId = element.getAttribute('data-l10n-id');
-    var l10nArgs = element.getAttribute('data-l10n-args');
-    var args = {};
-    if (l10nArgs) {
-      try {
-        args = JSON.parse(l10nArgs);
-      } catch (e) {
-        console.warn('could not parse arguments for ' + l10nId);
-      }
-    }
-    return { id: l10nId, args: args };
-  }
-
-  function setTextContent(element, text) {
-    // standard case: no element children
-    if (!element.firstElementChild) {
-      element.textContent = text;
-      return;
-    }
-
-    // this element has element children: replace the content of the first
-    // (non-blank) child textNode and clear other child textNodes
-    var found = false;
-    var reNotBlank = /\S/;
-    for (var child = element.firstChild; child; child = child.nextSibling) {
-      if (child.nodeType === 3 && reNotBlank.test(child.nodeValue)) {
-        if (found) {
-          child.nodeValue = '';
-        } else {
-          child.nodeValue = text;
-          found = true;
-        }
-      }
-    }
-    // if no (non-empty) textNode is found, insert a textNode before the
-    // element's first child.
-    if (!found) {
-      element.insertBefore(document.createTextNode(text), element.firstChild);
-    }
-  }
-
-  function translateNode(ctx, node) {
-    var attrs = getL10nAttributes(node);
-    if (!attrs.id) {
-      return true;
-    }
-
-    var entity = ctx.getEntity(attrs.id, attrs.args);
-    if (entity === null) {
-      return false;
-    }
-
-    if (entity.value) {
-      setTextContent(node, entity.value);
-    }
-
-    for (var key in entity.attributes) {
-      if (entity.attributes.hasOwnProperty(key)) {
-        var attr = entity.attributes[key];
-        var pos = key.indexOf('.');
-        if (pos !== -1) {
-          node[key.substr(0, pos)][key.substr(pos + 1)] = attr;
-        } else {
-          node[key] = attr;
-        }
-      }
-    }
-    return true;
-  }
-  
-  // localize an node as soon as ctx is ready
-  function localizeNode(ctx, element, id, args) {
-    if (!element) {
-      return;
-    }
-
-    if (!id) {
-      element.removeAttribute('data-l10n-id');
-      element.removeAttribute('data-l10n-args');
-      setTextContent(element, '');
-      return;
-    }
-
-    // set the data-l10n-[id|args] attributes
-    element.setAttribute('data-l10n-id', id);
-    if (args && typeof args === 'object') {
-      element.setAttribute('data-l10n-args', JSON.stringify(args));
-    } else {
-      element.removeAttribute('data-l10n-args');
-    }
-
-    // if ctx is ready, translate now;
-    // if not, the element will be translated along with the document anyway.
-    if (ctx.isReady) {
-      translateNode(ctx, element);
-    }
-  }
-  
-  // translate an array of HTML nodes
-  // -- returns an array of nodes that could not be translated
-  function translateNodes(ctx, elements) {
-    var untranslated = [];
-    for (var i = 0, l = elements.length; i < l; i++) {
-      if (!translateNode(ctx, elements[i])) {
-        untranslated.push(elements[i]);
-      }
-    }
-    return untranslated;
-  }
-
-  // translate an HTML subtree
-  // -- returns an array of elements that could not be translated
-  function translateFragment(ctx, element) {
-    element = element || document.documentElement;
-    var untranslated = translateNodes(ctx, getTranslatableChildren(element));
-    if (!translateNode(ctx, element)) {
-      untranslated.push(element);
-    }
-    return untranslated;
-  }
-
-  function fireLocalizedEvent(ctx) {
-    var event = document.createEvent('Event');
-    event.initEvent('localized', false, false);
-    event.language = ctx.supportedLocales[0];
-    window.dispatchEvent(event);
-  }
+function translateElement(element, id) {
+}
