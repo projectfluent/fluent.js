6c12637cc6b2d2d37e6f282bfc9a35c017c64e12
Zbigniew Braniecki <gandalf@mozilla.com>
2014-04-03 00:34:07 +0200

    switch integration tests from should to assert

--- tests/integration/basic.js
+++ tests/integration/basic.js
@@ -1,4 +1,5 @@
 'use strict';
+var assert = require('assert');
 
 var L20n = process.env.L20N_COV
   ? require('../../build/cov/lib/l20n')
@@ -15,31 +16,31 @@ describe('A simple context with linkResource', function() {
 
   it('should return the string value of brandName', function() {
     var value = ctx.get('brandName');
-    value.should.equal('Firefox');
+    assert.strictEqual(value, 'Firefox');
   });
   it('should return the value of about with the value of brandName in it', function() {
     var value = ctx.get('about');
-    value.should.equal('About Firefox');
+    assert.strictEqual(value, 'About Firefox');
   });
   it('should return the value of cert with the value of organization passed directly', function() {
     var value = ctx.get('cert', {organization: 'Mozilla Foundation'});
-    value.should.equal('Certificate signed by Mozilla Foundation');
+    assert.strictEqual(value, 'Certificate signed by Mozilla Foundation');
   });
   it('should return the correct plural form for 0', function() {
     var value = ctx.get('unreadMessages', {unread: 0});
-    value.should.equal('0 unread');
+    assert.strictEqual(value, '0 unread');
   });
   it('should return the correct plural form for 1', function() {
     var value = ctx.get('unreadMessages', {unread: 1});
-    value.should.equal('One unread');
+    assert.strictEqual(value, 'One unread');
   });
   it('should return the correct plural form for 2', function() {
     var value = ctx.get('unreadMessages', {unread: 2});
-    value.should.equal('2 unread');
+    assert.strictEqual(value, '2 unread');
   });
   it('should return the correct plural form for 3', function() {
     var value = ctx.get('unreadMessages', {unread: 3});
-    value.should.equal('3 unread');
+    assert.strictEqual(value, '3 unread');
   });
 });
 
