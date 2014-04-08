f1550f4ffd91a8c75c47c227f09165b538c27cc1
Zbigniew Braniecki <gandalf@mozilla.com>
2014-04-07 18:28:05 -0700

    some more intrumentation for jshint

--- tests/lib/context/loading_test.js
+++ tests/lib/context/loading_test.js
@@ -1,4 +1,4 @@
-/* global it, assert:true, describe,, beforeEach, navigator */
+/* global it, assert:true, describe, beforeEach, navigator */
 
 if (typeof navigator !== 'undefined') {
   var L10n = navigator.mozL10n._getInternalAPI();
@@ -23,23 +23,19 @@ describe('A non-loading context', function() {
     assert.throws(function(){
       ctx.get('dummy');
     }, /Context not ready/);
-  })
+  });
+
   it('should throw on getEntity', function() {
     assert.throws(function(){
       ctx.getEntity('dummy');
     }, /Context not ready/);
-  })
+  });
 });
 
 describe('A loading, non-ready context', function() {
   var ctx;
   beforeEach(function() {
     ctx = new Context();
-    if (typeof navigator !== 'undefined') {
-      var path = 'http://gallery.gaiamobile.org:8080/test/unit/l10n/lib/context';
-    } else {
-      var path = __dirname;
-    }
     ctx.resLinks.push(path + '/fixtures/strings.properties');
     ctx.requestLocales();
   });
@@ -48,33 +44,31 @@ describe('A loading, non-ready context', function() {
     assert.throws(function(){
       ctx.requestLocales();
     }, /Context not ready/);
-  })
+  });
+
   it('should throw on get', function() {
     assert.throws(function(){
       ctx.get('dummy');
     }, /Context not ready/);
-  })
+  });
+
   it('should throw on getEntity', function() {
     assert.throws(function(){
       ctx.getEntity('dummy');
     }, /Context not ready/);
-  })
+  });
+
   it('should throw on requestLocales', function() {
     assert.throws(function(){
       ctx.requestLocales('en-US');
     }, /Context not ready/);
-  })
+  });
 });
 
 describe('A loading, ready context', function() {
   var ctx;
   beforeEach(function(done) {
     ctx = new Context();
-    if (typeof navigator !== 'undefined') {
-      var path = 'http://gallery.gaiamobile.org:8080/test/unit/l10n/lib/context';
-    } else {
-      var path = __dirname;
-    }
     ctx.resLinks.push(path + '/fixtures/strings.properties');
     ctx.addEventListener('ready', function onReady() {
       ctx.removeEventListener('ready', onReady);
@@ -87,44 +81,43 @@ describe('A loading, ready context', function() {
     assert.doesNotThrow(function(){
       ctx.get('dummy');
     });
-  })
+  });
+
   it('should not throw on get of an unknown entity', function() {
     assert.doesNotThrow(function(){
       ctx.get('missing');
     });
-  })
+  });
+
   it('should not throw on getEntity of a known entity', function() {
     assert.doesNotThrow(function(){
       ctx.getEntity('dummy');
     });
-  })
+  });
+
   it('should not throw on getEntity of an unknown entity', function() {
     assert.doesNotThrow(function(){
       ctx.getEntity('missing');
     });
-  })
+  });
+
   it('should not throw on requestLocales', function() {
     assert.doesNotThrow(function(){
       ctx.requestLocales('en-US');
     });
-  })
+  });
 });
 
 describe('A loading, ready context', function() {
   var ctx;
   beforeEach(function(done) {
     ctx = new Context();
-    if (typeof navigator !== 'undefined') {
-      var path = 'http://gallery.gaiamobile.org:8080/test/unit/l10n/lib/context';
-    } else {
-      var path = __dirname;
-    }
     ctx.resLinks.push(path + '/fixtures/{{locale}}.properties');
     ctx.once(done);
     ctx.requestLocales('en-US');
   });
 
-  it('should return translations for the current built fallback chain ', function() {
+  it('should return translations for the built fallback chain ', function() {
     // Bug 942183 - Error when localizeNode is done quickly after
     // requestLocales https://bugzil.la/942183
     // Changing locales triggers the build process for the 'pl' locale.
