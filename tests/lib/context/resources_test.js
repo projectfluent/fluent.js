7f2438e914e2442d6f4b74d30bd414fe2dd25a85
Staś Małolepszy <stas@mozilla.com>
2014-04-08 04:08:28 +0200

    explicit assertion

--- tests/lib/context/resources_test.js
+++ tests/lib/context/resources_test.js
@@ -1,10 +1,11 @@
-/* global it, describe, beforeEach, navigator */
+/* global assert:true, it, describe, beforeEach, navigator */
 
 if (typeof navigator !== 'undefined') {
   var L10n = navigator.mozL10n._getInternalAPI();
   var Context = L10n.Context;
   var path = 'http://gallery.gaiamobile.org:8080/test/unit/l10n/context';
 } else {
+  assert = require('assert');
   var Context = process.env.L20N_COV ?
     require('../../../build/cov/lib/l20n/context').Context
     : require('../../../lib/l20n/context').Context;
@@ -16,16 +17,16 @@ describe('Missing resources', function() {
 
   var ctx;
 
-  beforeEach(function() {
+  beforeEach(function(done) {
     ctx = new Context();
-
     ctx.resLinks.push(path + '/fixtures/en-US.properties');
     ctx.resLinks.push(path + '/fixtures/missing.properties');
-  });
-
-  it('should get ready', function(done) {
     ctx.once(done);
     ctx.requestLocales();
+  });
+
+  it('should get ready', function() {
+    assert.strictEqual(ctx.isReady, true);
   });
 });
 
@@ -34,14 +35,15 @@ describe('No valid resources', function() {
 
   var ctx;
 
-  beforeEach(function() {
+  beforeEach(function(done) {
     ctx = new Context();
     ctx.resLinks.push(path + '/fixtures/missing.properties');
     ctx.resLinks.push(path + '/fixtures/another.properties');
-  });
-
-  it('should get ready', function(done) {
     ctx.once(done);
     ctx.requestLocales();
+  });
+
+  it('should get ready', function() {
+    assert.strictEqual(ctx.isReady, true);
   });
 });
