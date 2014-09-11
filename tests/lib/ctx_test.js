0ee6a750d81c62fec067913fa650978acea701ff
Staś Małolepszy <stas@mozilla.com>
2014-09-11 21:38:39 +0200

    Rename l10n.getContext to l10n.require;  add ctx.destroy

--- tests/lib/ctx_test.js
+++ tests/lib/ctx_test.js
@@ -13,7 +13,7 @@ if (typeof navigator !== 'undefined') {
     : require('../../lib/l20n/env').Env;
 }
 
-describe('ctx.get', function() {
+describe('Context', function() {
   var l10n;
 
   beforeEach(function(done) {
@@ -38,11 +38,65 @@ describe('ctx.get', function() {
     });
   });
 
-  it('returns the value from the AST', function(done) {
-    var ctx = l10n.getContext(['res1', 'res2']);
-    ctx.get('foo').then(function(val) {
-      assert.strictEqual(val, 'this is fooundefined');
-      done();
+  describe('ctx.loaded', function() {
+    it('is a promise', function(done) {
+      var ctx = l10n.require(['res1', 'res2']);
+      ctx.loaded.then(function() {
+        done();
+      });
+    });
+  });
+
+  describe('ctx.get', function() {
+    it('returns the value from the AST', function(done) {
+      var ctx = l10n.require(['res1', 'res2']);
+      ctx.get('foo').then(function(val) {
+        assert.strictEqual(val, 'this is fooundefined');
+        done();
+      });
+    });
+  });
+
+  describe('ctx.destroy', function() {
+    var ctx1, ctx2;
+
+    beforeEach(function(done) {
+      ctx1 = l10n.require(['res1', 'res2']);
+      ctx1.loaded.then(function() {
+        done();
+      });
+    });
+
+    it('removes the ctx from _ctxMap', function() {
+      // XXX should it be possible to destroy contexts before they load?
+      ctx1.destroy();
+      assert.ok(
+        !l10n._ctxMap.has(ctx1),
+        'expected ctx to be removed from l10n._ctxMap');
+    });
+    it('removes the resources from _resCache', function() {
+      ctx1.destroy();
+      assert.ok(
+        !l10n._resCache.res1,
+        'expected res1 to be removed from l10n._resCache');
+      assert.ok(
+        !l10n._resCache.res2,
+        'expected res2 to be removed from l10n._resCache');
+    });
+    it('removes the resources uniquely associated with the ctx',
+       function(done) {
+      ctx2 = l10n.require(['res1', 'res3']);
+        ctx1.destroy();
+        assert.ok(
+          l10n._resCache.res1,
+          'expected res1 to be defined in l10n._resCache');
+        assert.ok(
+          !l10n._resCache.res2,
+          'expected res2 to be removed from l10n._resCache');
+        assert.ok(
+          l10n._resCache.res3,
+          'expected res1 to be defined in l10n._resCache');
+        done();
     });
   });
 
