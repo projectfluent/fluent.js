cc27a1f1c4cee3287e8a9aacc38fae32ce6da8c9
Staś Małolepszy <stas@mozilla.com>
2014-09-18 13:07:43 +0200

    Revert "add requested back into the Env's ctor"
    This reverts commit 74fb43115520b381bbbcbdfcb17d419db5f97271.

--- tests/lib/env/langs_test.js
+++ tests/lib/env/langs_test.js
@@ -35,33 +35,48 @@ describe('Creating Envs', function() {
   });
 
   it('correctly sets the default language', function(done) {
-    l10n.registered.then(function() {
+    l10n.ready.then(function() {
       assert.strictEqual(l10n.default, 'en-US');
     }).then(done, done);
   });
 
   it('corectly sets the available languages', function(done) {
-    l10n.registered.then(function() {
+    l10n.ready.then(function() {
       assert.deepEqual(l10n.available, ['pl', 'de', 'en-US']);
     }).then(done, done);
   });
 
   it('correctly sets the supported languages', function(done) {
-    l10n.supported.then(function(supported) {
-      assert.deepEqual(supported, ['pl', 'en-US']);
+    l10n.ready.then(function() {
+      assert.deepEqual(l10n.supported, ['pl', 'en-US']);
     }).then(done, done);
+  });
+
+  it('emits the availablelanguageschange event', function(done) {
+    l10n.addEventListener('availablelanguageschange', function(available) {
+      assert.deepEqual(available, ['pl', 'de', 'en-US']);
+      done();
+    });
+  });
+
+  it('emits the languagechange event', function(done) {
+    l10n.addEventListener('languagechange', function(supported) {
+      assert.deepEqual(supported, ['pl', 'en-US']);
+      done();
+    });
   });
 
   describe('Requesting new languages', function() {
 
     beforeEach(function(done) {
-      l10n.supported.then(done.bind(null, null));
+      l10n.ready.then(done.bind(null, null));
     });
 
     it('correctly sets the supported languages', function(done) {
-      l10n.request(['de']).then(function(supported) {
-        assert.deepEqual(supported, ['de', 'en-US']);
+      l10n.request(['de']).then(function() {
+        assert.deepEqual(l10n.supported, ['de', 'en-US']);
       }).then(done, done);
+      assert.deepEqual(l10n.supported, ['pl', 'en-US']);
     });
 
     it('emits the languagechange event', function(done) {
