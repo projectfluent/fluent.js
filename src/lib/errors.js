48d04c21c38349b7dd06feb333925beecf54b34c
Staś Małolepszy <stas@mozilla.com>
2015-05-30 11:53:02 +0200

    rename properties of L10nError

--- src/lib/errors.js
+++ src/lib/errors.js
@@ -1,10 +1,10 @@
 'use strict';
 
-export function L10nError(message, id, loc) {
+export function L10nError(message, id, code) {
   this.name = 'L10nError';
   this.message = message;
   this.id = id;
-  this.loc = loc;
+  this.code = code;
 }
 L10nError.prototype = Object.create(Error.prototype);
 L10nError.prototype.constructor = L10nError;
