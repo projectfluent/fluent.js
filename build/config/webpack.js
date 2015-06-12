cbd9c5162833fe5d84e4224ea28b4f016d231760
Staś Małolepszy <stas@mozilla.com>
2015-06-12 14:57:57 +0200

    clean up webpack, add polyfill to examples

--- build/config/webpack.js
+++ build/config/webpack.js
@@ -3,63 +3,55 @@
 // resolve relative to build/config
 var path = require('path').resolve.bind(null, __dirname);
 
+var babel = {
+  test: /\.js$/,
+  include: [
+    path('../../src')
+  ],
+  loader: 'babel',
+  query: {
+    comments: false,
+    whitelist: [
+      'strict',
+      'es6.modules',
+      'es6.classes',
+      'es6.destructuring',
+      'es6.arrowFunctions',
+      'es6.properties.shorthand',
+      'es6.forOf',
+      'es6.spread',
+      'es6.parameters.rest',
+      'es6.parameters.default',
+      'es6.blockScoping'
+    ],
+  }
+};
+
 module.exports = {
   web: {
     context: path('../../src'),
-    entry: {
-      web: './runtime/web/index.js',
-    },
+    entry: './runtime/web/index.js',
     output: {
       path: path('../dist'),
-      filename: '[name]/l20n.js',
+      filename: 'web/l20n.js',
       libraryTarget: 'this',
 
     },
     module: {
-      loaders: [{ 
-        test: /\.js$/,
-        include: [
-          path('../../src')
-        ],
-        loader: 'babel',
-      }]
+      loaders: [babel]
     }
   },
   gaia: {
     context: path('../../src'),
-    entry: {
-      gaia: './runtime/web/index.js',
-    },
+    entry: './runtime/web/index.js',
     output: {
       path: path('../dist'),
-      filename: '[name]/l20n.js',
+      filename: 'gaia/l20n.js',
       libraryTarget: 'this',
 
     },
     module: {
-      loaders: [{ 
-        test: /\.js$/,
-        include: [
-          path('../../src')
-        ],
-        loader: 'babel',
-        query: {
-          comments: false,
-          whitelist: [
-            'strict',
-            'es6.modules',
-            'es6.classes',
-            'es6.destructuring',
-            'es6.arrowFunctions',
-            'es6.properties.shorthand',
-            'es6.forOf',
-            'es6.spread',
-            'es6.parameters.rest',
-            'es6.parameters.default',
-            'es6.blockScoping'
-          ],
-        }
-      }]
+      loaders: [babel]
     }
   },
   node: {
@@ -67,20 +59,14 @@ module.exports = {
     entry: './runtime/node/index.js',
     output: {
       path: path('../dist'),
-      filename: './node/l20n.js',
+      filename: 'node/l20n.js',
       libraryTarget: 'commonjs2',
     },
     externals: {
       'fs': true
     },
     module: {
-      loaders: [{ 
-        test: /\.js$/,
-        include: [
-          path('../../src')
-        ],
-        loader: 'babel'
-      }]
+      loaders: [babel]
     }
   },
 };
