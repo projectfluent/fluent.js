6b97da5acd370d501654ebe1a971fd913c921b88
Staś Małolepszy <stas@mozilla.com>
2015-06-23 16:46:04 -0700

    tests/lib/env

--- tests/lib/env/resCache.js
+++ tests/lib/env/resCache.js
@@ -1,25 +1,15 @@
 /* global Promise, assert:true, it, describe, beforeEach */
-/* global navigator, __dirname */
 'use strict';
 
-if (typeof navigator !== 'undefined') {
-  var L20n = navigator.mozL10n._getInternalAPI();
-  var path =
-    'app://sharedtest.gaiamobile.org/test/unit/l10n/lib';
-} else {
-  var assert = require('assert');
-  var L20n = {
-    Env: require('../../../src/lib/env').Env,
-    fetch: require('../../../src/runtime/node/io').fetch,
-    L10nError: require('../../../src/lib/errors').L10nError
-  };
-  var path = __dirname + '/..';
-}
+import assert from 'assert';
+import { Env } from '../../../src/lib/env';
+import { fetch } from '../../../src/runtime/node/io';
+import { L10nError } from '../../../src/lib/errors';
 
-var langs = [
+const path = __dirname + '/..';
+const langs = [
   { code: 'en-US', src: 'app', dir: 'ltr' },
 ];
-
 
 describe('Caching resources', function() {
   var env, ctx1, ctx2;
@@ -28,7 +18,7 @@ describe('Caching resources', function() {
   var res3 = path + '/fixtures/missing.properties';
 
   beforeEach(function(done) {
-    env = new L20n.Env('en-US', L20n.fetch);
+    env = new Env('en-US', fetch);
     ctx1 = env.createContext([res1, res3]);
     ctx2 = env.createContext([res1, res2]);
     Promise.all([
@@ -42,7 +32,7 @@ describe('Caching resources', function() {
   it('caches resources', function() {
     assert(env._resCache[res1 + 'en-USapp']);
     assert(env._resCache[res2 + 'en-USapp']);
-    assert(env._resCache[res3 + 'en-USapp'] instanceof L20n.L10nError);
+    assert(env._resCache[res3 + 'en-USapp'] instanceof L10nError);
   });
 
   // destroyContext has been removed in PR #39
@@ -52,7 +42,7 @@ describe('Caching resources', function() {
     env.destroyContext(ctx2);
     assert(env._resCache[res1]['en-US'].app);
     assert(!env._resCache[res2]);
-    assert(env._resCache[res3]['en-US'].app instanceof L20n.L10nError);
+    assert(env._resCache[res3]['en-US'].app instanceof L10nError);
   });
 
 });
