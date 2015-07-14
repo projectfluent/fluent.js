e49fd31bebde60832ed764ed087c4bb3149d5c6f
Zibi Braniecki <gandalf@mozilla.com>
2015-07-14 09:43:59 -0700

    Remove compatibility layer with navigator.mozL10n

--- src/runtime/web/index.js
+++ src/runtime/web/index.js
@@ -2,7 +2,6 @@
 
 import { fetch } from './io';
 import { Service } from '../../bindings/html/service';
-import { setAttributes, getAttributes } from '../../bindings/html/dom';
 
 const readyStates = {
   loading: 0,
@@ -31,15 +30,3 @@ function init() {
 }
 
 whenInteractive(init);
-
-// XXX for easier testing with existing Gaia apps; remove later on
-const once = callback => whenInteractive(
-  () => document.l10n.ready.then(callback));
-
-navigator.mozL10n = {
-  get: id => id,
-  once: once,
-  ready: once,
-  setAttributes: setAttributes,
-  getAttributes: getAttributes
-};
