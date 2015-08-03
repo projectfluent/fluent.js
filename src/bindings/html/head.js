f4f24c7dac162bf01982aaf0e8b2179a8a1918d0
Staś Małolepszy <stas@mozilla.com>
2015-08-03 23:47:32 +0200

    remove html/polyfill for now; more the code into html/head

--- src/bindings/html/head.js
+++ src/bindings/html/head.js
@@ -1,6 +1,10 @@
 'use strict';
 
-import './polyfill';
+// Polyfill NodeList.prototype[Symbol.iterator] for Chrome.
+// See https://code.google.com/p/chromium/issues/detail?id=401699
+if (!NodeList.prototype[Symbol.iterator]) {
+  NodeList.prototype[Symbol.iterator] = Array.prototype[Symbol.iterator];
+}
 
 export function getResourceLinks(head) {
   return Array.prototype.map.call(
--- src/bindings/html/polyfill.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-// Polyfill NodeList.prototype[Symbol.iterator] for Chrome.
-// See https://code.google.com/p/chromium/issues/detail?id=401699
-if (!NodeList.prototype[Symbol.iterator]) {
-  NodeList.prototype[Symbol.iterator] = Array.prototype[Symbol.iterator];
-}
