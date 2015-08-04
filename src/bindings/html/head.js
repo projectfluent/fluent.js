ee4859ff1d2466d0d164673712abe2daca7b8170
Staś Małolepszy <stas@mozilla.com>
2015-08-05 00:44:13 +0200

    Check if NodeList is defined

--- src/bindings/html/head.js
+++ src/bindings/html/head.js
@@ -2,7 +2,7 @@
 
 // Polyfill NodeList.prototype[Symbol.iterator] for Chrome.
 // See https://code.google.com/p/chromium/issues/detail?id=401699
-if (!NodeList.prototype[Symbol.iterator]) {
+if (typeof NodeList === 'function' && !NodeList.prototype[Symbol.iterator]) {
   NodeList.prototype[Symbol.iterator] = Array.prototype[Symbol.iterator];
 }
 
