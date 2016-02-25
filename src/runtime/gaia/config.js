ab671091b0f2495ca6aa48506e81da347d6ff1fb
Zibi Braniecki <gandalf@mozilla.com>
2016-02-24 16:30:20 -0800

    Remove gaia target. r=stas

--- src/runtime/gaia/config.js
+++ src/runtime/gaia/config.js
@@ -1,11 +1,6 @@
 /* jshint node:true */
 
 module.exports = {
-  gaia: {
-    files: {
-      'dist/bundle/gaia/l20n.js': 'src/runtime/gaia/index.js',
-    }
-  },
   gaiabuild: {
     options: {
       format: 'cjs',
