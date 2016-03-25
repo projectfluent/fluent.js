a391a798e68fefd44fecc5a478ac229fd852551e
Staś Małolepszy <stas@mozilla.com>
2016-03-25 21:49:56 +0100

    fix linting error, my bad

--- src/runtime/node/index.js
+++ src/runtime/node/index.js
@@ -4,7 +4,12 @@ import 'string.prototype.endswith';
 export { fetchResource } from './io';
 export { Env } from '../../lib/env';
 
-export { default as PropertiesParser } from '../../lib/format/properties/parser';
-export { default as L20nParser } from '../../lib/format/l20n/entries/parser';
+export { 
+  default as PropertiesParser
+} from '../../lib/format/properties/parser';
+export {
+  default as L20nParser
+} from '../../lib/format/l20n/entries/parser';
+
 export { format } from '../../lib/resolver';
 export * from '../../lib/mocks';
