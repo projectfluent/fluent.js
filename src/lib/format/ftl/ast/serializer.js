ec86887167cd1dbbe7e7d131edcaa25699bcd0f7
Zibi Braniecki <gandalf@mozilla.com>
2016-03-30 16:15:29 -0700

    Support simple html attributes

--- src/lib/format/ftl/ast/serializer.js
+++ src/lib/format/ftl/ast/serializer.js
@@ -19,14 +19,30 @@ export default {
     let id = this.dumpIdentifier(entity.id);
     let value = this.dumpPattern(entity.value);
 
-    return `${id} = ${value}`;
+    if (entity.traits.length) {
+      let traits = this.dumpTraits(entity.traits, 2);
+      return `${id} = ${value}\n${traits}`;
+    } else {
+      return `${id} = ${value}`;
+    }
   },
 
   dumpIdentifier: function(id) {
+    if (id.namespace) {
+      return `${id.namespace}:${id.name}`;
+    }
     return id.name;
   },
 
   dumpPattern: function(pattern) {
     return pattern.source;
   },
+
+  dumpTraits: function(traits, indent) {
+    return traits.map(trait => {
+      let id = this.dumpIdentifier(trait.key);
+      let value = this.dumpPattern(trait.value);
+      return `${' '.repeat(indent)}[${id}] ${value}`;
+    }).join('\n');
+  }
 }
--- tools/extract-strings.js
+++ tools/extract-strings.js
@@ -11,16 +11,27 @@ require('babel-register')({
 const AST = require('../src/lib/format/ftl/ast/ast').default;
 const Serializer = require('../src/lib/format/ftl/ast/serializer').default;
 
-
 function extractFromHTML(err, data) {
   const res = new AST.Resource();
 
   const $ = cheerio.load(data.toString());
   const elements = $('*[data-l10n-id]');
   elements.each(function(index, element) {
+
     const id = new AST.Identifier($(this).attr('data-l10n-id'));
     const value = new AST.Pattern($(this).text());
-    res.body.push(new AST.Entity(id, value));
+
+    const traits = [];
+
+    for (let i in element.attribs) {
+      if (i === 'title') {
+        const id = new AST.Identifier(i, 'html');
+        const value = new AST.Pattern($(this).attr(i));
+        const trait = new AST.Member(id, value);
+        traits.push(trait);
+      }
+    }
+    res.body.push(new AST.Entity(id, value, traits));
   });
 
   console.log(Serializer.serialize(res));
