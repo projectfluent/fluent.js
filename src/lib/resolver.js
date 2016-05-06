a2d5d89e5041b8e64ef6f377e687109093f72bbf
Staś Małolepszy <stas@mozilla.com>
2016-05-07 00:10:58 +0200

    ReadWrite monad

--- src/lib/resolver.js
+++ src/lib/resolver.js
@@ -1,4 +1,5 @@
 import { L10nError } from './errors';
+import { resolve, ask, tell, fail } from './readwrite';
 import builtins, {
   FTLNumber, FTLDateTime, FTLKeyword, FTLList
 } from './builtins';
@@ -9,261 +10,254 @@ const PDI = '\u2069';
 
 const MAX_PLACEABLE_LENGTH = 2500;
 
-function mapValues(rc, arr) {
-  return arr.reduce(
-    ([valseq, errseq], cur) => {
-      const [value, errs] = Value(rc, cur);
-      return [valseq.concat(value), errseq.concat(errs)];
-    }, [new FTLList(), []]
-  );
+function* mapValues(arr) {
+  let values = new FTLList();
+  for (let elem of arr) {
+    values.push(yield* Value(elem));
+  }
+  return values;
 }
-
-function unit(val) {
-  return [val, []];
-}
-
-function fail(val, err) {
-  return [val, [err]];
-}
-
-function flat([val, errs2], errs1) {
-  return [val, [...errs1, ...errs2]];
-}
-
 
 // Helper for choosing entity value
 
-function DefaultMember(members) {
+function* DefaultMember(members) {
   for (let member of members) {
     if (member.def) {
-      return unit(member);
+      return yield member;
     }
   }
 
-  return fail('???', new L10nError('No default'));
+  return yield fail('???', new L10nError('No default'));
 }
 
 
 // Half-resolved expressions
 
-function Expression(rc, expr) {
+function* Expression(expr) {
   switch (expr.type) {
     case 'ref':
-      return EntityReference(rc, expr);
+      return yield* EntityReference(expr);
     case 'blt':
-      return BuiltinReference(rc, expr);
+      return yield* BuiltinReference(expr);
     case 'mem':
-      return MemberExpression(rc, expr);
+      return yield* MemberExpression(expr);
     case 'sel':
-      return SelectExpression(rc, expr);
+      return yield* SelectExpression(expr);
     default:
-      return unit(expr);
+      return yield expr;
   }
 }
 
-function EntityReference(rc, expr) {
+function* EntityReference(expr) {
+  const rc = yield ask();
   const entity = rc.ctx._getEntity(rc.lang, expr.name);
 
   if (!entity) {
-    return fail(expr.name, new L10nError('Unknown entity: ' + expr.name));
+    return yield fail(expr.name, new L10nError('Unknown entity: ' + expr.name));
   }
 
-  return unit(entity);
+  return yield entity;
 }
 
-function BuiltinReference(rc, expr) {
+function* BuiltinReference(expr) {
   const builtin = builtins[expr.name];
 
   if (!builtin) {
-    return fail(
+    return yield fail(
       expr.name + '()', new L10nError('Unknown built-in: ' + expr.name + '()')
     );
   }
 
-  return unit(builtin);
+  return yield builtin;
 }
 
-function MemberExpression(rc, expr) {
-  const [entity, errs] = Expression(rc, expr.obj);
-  if (errs.length) {
-    return [entity, errs];
-  }
-
-  const [key] = Value(rc, expr.key);
+function* MemberExpression(expr) {
+  const entity = yield* Expression(expr.obj);
+  const key = yield* Value(expr.key);
+  const rc = yield ask();
 
   for (let member of entity.traits) {
-    const [memberKey] = Value(rc, member.key);
+    const memberKey = yield* Value(member.key);
     if (key.match(rc, memberKey)) {
-      return unit(member);
+      return yield member;
     }
   }
 
-  return fail(entity, new L10nError('Unknown trait: ' + key.toString(rc)));
+  return yield fail(entity, new L10nError('Unknown trait: ' + key.toString(rc)));
 }
 
-function SelectExpression(rc, expr) {
-  const [selector, errs] = Value(rc, expr.exp);
-  if (errs.length) {
-    return flat(DefaultMember(expr.vars), errs);
-  }
+function* SelectExpression(expr) {
+  const selector = yield* Value(expr.exp);
+  // if (errs.length) {
+  //   return flat(DefaultMember(expr.vars), errs);
+  // }
 
   for (let variant of expr.vars) {
-    const [key] = Value(rc, variant.key);
+    const key = yield* Value(variant.key);
 
     if (key instanceof FTLNumber &&
         selector instanceof FTLNumber &&
         key.valueOf() === selector.valueOf()) {
-      return unit(variant);
+      return yield variant;
     }
+
+    const rc = yield ask();
 
     if (key instanceof FTLKeyword &&
         key.match(rc, selector)) {
-      return unit(variant);
+      return yield variant;
     }
   }
 
-  return DefaultMember(expr.vars);
+  return yield* DefaultMember(expr.vars);
 }
 
 
 // Fully-resolved expressions
 
-function Value(rc, expr) {
+function* Value(expr) {
   if (typeof expr === 'string' || expr === null) {
-    return unit(expr);
+    return yield expr;
   }
 
   if (Array.isArray(expr)) {
-    return Pattern(rc, expr);
+    return yield* Pattern(expr);
   }
 
-  const [node, errs] = Expression(rc, expr);
-  if (errs.length) {
-    // Expression short-circuited into a simple string or a fallback
-    return flat(Value(rc, node), errs);
-  }
+  const node = yield* Expression(expr);
+  // if (errs.length) {
+  //   // Expression short-circuited into a simple string or a fallback
+  //   return flat(Value(rc, node), errs);
+  // }
 
   switch (node.type) {
     case 'kw':
-      return [new FTLKeyword(node), errs];
+      return yield new FTLKeyword(node);
     case 'num':
-      return [new FTLNumber(node.val), errs];
+      return yield new FTLNumber(node.val);
     case 'ext':
-      return flat(ExternalArgument(rc, node), errs);
+      return yield* ExternalArgument(node);
     case 'call':
-      return flat(CallExpression(rc, expr), errs);
+      return yield* CallExpression(expr);
     default:
       return node.key ? // is it a Member?
-        flat(Value(rc, node.val), errs) :
-        flat(Entity(rc, node), errs);
+        yield* Value(node.val) :
+        yield* Entity(node);
   }
 }
 
-function ExternalArgument(rc, expr) {
+function* ExternalArgument(expr) {
   const name = expr.name;
-  const args = rc.args;
+  const { args } = yield ask();
 
   if (!args || !args.hasOwnProperty(name)) {
-    return fail(name, new L10nError('Unknown external: ' + name));
+    return yield fail(name, new L10nError('Unknown external: ' + name));
   }
 
   const arg = args[name];
 
   switch (typeof arg) {
     case 'string':
-      return unit(arg);
+      return yield arg;
     case 'number':
-      return unit(new FTLNumber(arg));
+      return yield new FTLNumber(arg);
     case 'object':
       if (Array.isArray(arg)) {
-        return mapValues(rc, arg);
+        return yield* mapValues(arg);
       }
       if (arg instanceof Date) {
-        return unit(new FTLDateTime(arg));
+        return yield new FTLDateTime(arg);
       }
     default:
-      return fail(name, new L10nError(
+      return yield fail(name, new L10nError(
         'Unsupported external type: ' + name + ', ' + typeof arg
       ));
   }
 }
 
-function CallExpression(rc, expr) {
-  const [callee, errs1] = Expression(rc, expr.name);
-  if (errs1.length) {
-    return [callee, errs1];
+function* CallExpression(expr) {
+  const callee = yield* Expression(expr.name);
+
+  // if (errs1.length) {
+  //   return [callee, errs1];
+  // }
+
+  let pargs = [];
+  let kargs = [];
+
+  for (let arg of expr.args) {
+    if (arg.type === 'kv') {
+      const val = yield* Value(arg.val);
+      kargs[arg.name] = val;
+    } else {
+      const val = yield* Value(arg);
+      pargs.push(val);
+    }
   }
 
-  const [pargs, kargs, errs2] = expr.args.reduce(
-    ([pargseq, kargseq, errseq], arg) => {
-      if (arg.type === 'kv') {
-        const [val, errs] = Value(rc, arg.val);
-        kargseq[arg.name] = val;
-        return [pargseq, kargseq, [...errseq, ...errs]];
-      } else {
-        const [val, errs] = Value(rc, arg);
-        return [[...pargseq, val], kargseq, [...errseq, ...errs]];
-      }
-    }, [[], {}, []]);
-  return [callee(pargs, kargs), errs2];
+  return yield callee(pargs, kargs);
 }
 
-function Pattern(rc, ptn) {
+function* Pattern(ptn) {
+  const rc = yield ask();
+
   if (rc.dirty.has(ptn)) {
-    return fail('???', new L10nError('Cyclic reference'));
+    return yield fail('???', new L10nError('Cyclic reference'));
   }
 
   rc.dirty.add(ptn);
 
-  const rv = ptn.reduce(([valseq, errseq], elem) => {
-    if (typeof elem === 'string') {
-      return [valseq + elem, errseq];
+  let result = '';
+
+  for (let part of ptn) {
+    if (typeof part === 'string') {
+      result += part;
     } else {
-      const [value, errs] = elem.length === 1 ?
-        Value(rc, elem[0]) : mapValues(rc, elem);
+      const value = part.length === 1 ?
+        yield* Value(part[0]) : yield* mapValues(part);
+
       const str = value.toString(rc);
       if (str.length > MAX_PLACEABLE_LENGTH) {
-        return [
-          valseq + '???',
-          [...errseq, ...errs, new L10nError(
-            'Too many characters in placeable ' +
-            `(${str.length}, max allowed is ${MAX_PLACEABLE_LENGTH})`
-          )]
-        ];
+        yield tell(new L10nError(
+          'Too many characters in placeable ' +
+          `(${str.length}, max allowed is ${MAX_PLACEABLE_LENGTH})`
+        ));
+        result += '???';
+      } else {
+        result += FSI + str + PDI;
       }
-      return [
-        valseq + FSI + str + PDI, [...errseq, ...errs],
-      ];
     }
-
-  }, ['', []]);
+  }
 
   rc.dirty.delete(ptn);
-  return rv;
+  return yield result;
 }
 
-function Entity(rc, entity) {
+function* Entity(entity) {
   if (!entity.traits) {
-    return Value(rc, entity);
+    return yield* Value(entity);
   }
 
   if (entity.val !== undefined) {
-    return Value(rc, entity.val);
+    return yield* Value(entity.val);
   }
 
-  const [def, errs] = DefaultMember(entity.traits);
-  return flat(Value(rc, def), errs);
+  const def = yield* DefaultMember(entity.traits);
+  return yield* Value(def);
 }
 
 
 export function format(ctx, lang, args, entity) {
-  // rc is the current resolution context
-  const rc = {
+  const res = resolve(function* () {
+    return yield* Value(entity);
+  }());
+
+  const ret = res.run({
     ctx,
     lang,
     args,
     dirty: new WeakSet()
-  };
+  });
 
-  return Value(rc, entity);
+  return ret;
 }
