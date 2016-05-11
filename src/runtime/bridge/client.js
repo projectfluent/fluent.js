0c2305693ac38dc532f473a76cb418beed9c1569
Staś Małolepszy <stas@mozilla.com>
2016-05-11 10:31:49 +0200

    Remove mozL10n proxy

--- src/runtime/bridge/client.js
+++ src/runtime/bridge/client.js
@@ -12,17 +12,3 @@ document.l10n = new View(client, document);
 window.addEventListener('pageshow', () => client.connect());
 window.addEventListener('pagehide', () => client.disconnect());
 window.addEventListener('languagechange', document.l10n);
-
-//Bug 1204660 - Temporary proxy for shared code. Will be removed once
-//              l10n.js migration is completed.
-navigator.mozL10n = {
-  setAttributes: document.l10n.setAttributes,
-  getAttributes: document.l10n.getAttributes,
-  formatValue: (...args) => document.l10n.formatValue(...args),
-  translateFragment: (...args) => document.l10n.translateFragment(...args),
-  once: cb => document.l10n.ready.then(cb),
-  ready: cb => document.l10n.ready.then(() => {
-    document.addEventListener('DOMRetranslated', cb);
-    cb();
-  }),
-};
