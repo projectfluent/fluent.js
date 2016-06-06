d97864d39440876301347960348c1f4894e23d67
Staś Małolepszy <stas@mozilla.com>
2016-06-06 11:37:16 +0200

    Make the allowed-list language-specific

--- src/bindings/dom/html.js
+++ src/bindings/dom/html.js
@@ -26,7 +26,38 @@ const allowed = {
 
 export class HTMLLocalization extends Localization {
   overlayElement(element, translation) {
-    // XXX make it use allowed from above
-    return overlayElement(element, translation);
+    return overlayElement(this, element, translation);
   }
+
+  // XXX the allowed list should be amendable; https://bugzil.la/922573
+  isElementAllowed(element) {
+    return allowed.elements.indexOf(element.tagName.toLowerCase()) !== -1;
+  }
+
+  isAttrAllowed(attr, element) {
+    const attrName = attr.name.toLowerCase();
+    const tagName = element.tagName.toLowerCase();
+    // is it a globally safe attribute?
+    if (allowed.attributes.global.indexOf(attrName) !== -1) {
+      return true;
+    }
+    // are there no allowed attributes for this element?
+    if (!allowed.attributes[tagName]) {
+      return false;
+    }
+    // is it allowed on this element?
+    // XXX the allowed list should be amendable; https://bugzil.la/922573
+    if (allowed.attributes[tagName].indexOf(attrName) !== -1) {
+      return true;
+    }
+    // special case for value on inputs with type button, reset, submit
+    if (tagName === 'input' && attrName === 'value') {
+      const type = element.type.toLowerCase();
+      if (type === 'submit' || type === 'button' || type === 'reset') {
+        return true;
+      }
+    }
+    return false;
+  }
+
 }
--- src/bindings/dom/xul.js
+++ src/bindings/dom/xul.js
@@ -2,9 +2,8 @@ import { Localization } from './base';
 import { overlayElement } from './overlay';
 
 const allowed = {
-  elements: [],
   attributes: {
-    global: ['title', 'aria-label', 'aria-valuetext', 'aria-moz-hint'],
+    global: ['aria-label', 'aria-valuetext', 'aria-moz-hint'],
     button: ['accesskey'],
     tab: ['label'],
     textbox: ['placeholder'],
@@ -13,7 +12,29 @@ const allowed = {
 
 export class XULLocalization extends Localization {
   overlayElement(element, translation) {
-    // XXX make it use allowed from above
-    return overlayElement(element, translation);
+    return overlayElement(this, element, translation);
+  }
+
+  isElementAllowed(element) {
+    return false;
+  }
+
+  isAttrAllowed(attr, element) {
+    const attrName = attr.name.toLowerCase();
+    const tagName = element.tagName.toLowerCase();
+    // is it a globally safe attribute?
+    if (allowed.attributes.global.indexOf(attrName) !== -1) {
+      return true;
+    }
+    // are there no allowed attributes for this element?
+    if (!allowed.attributes[tagName]) {
+      return false;
+    }
+    // is it allowed on this element?
+    // XXX the allowed list should be amendable; https://bugzil.la/922573
+    if (allowed.attributes[tagName].indexOf(attrName) !== -1) {
+      return true;
+    }
+    return false;
   }
 }
