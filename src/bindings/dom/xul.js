fa4d3f28b338f6fd30a08cbf6ff4a538a3eeffc0
Staś Małolepszy <stas@mozilla.com>
2016-06-09 15:47:49 +0200

    Use element.localName and check namespaceURI

--- src/bindings/dom/xul.js
+++ src/bindings/dom/xul.js
@@ -3,6 +3,8 @@ import { overlayElement } from './overlay';
 
 export { contexts } from './base';
 
+const ns = 'http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul';
+
 const allowed = {
   attributes: {
     global: ['aria-label', 'aria-valuetext', 'aria-moz-hint'],
@@ -22,21 +24,29 @@ export class XULLocalization extends Localization {
   }
 
   isAttrAllowed(attr, element) {
-    const attrName = attr.name.toLowerCase();
-    const tagName = element.tagName.toLowerCase();
+    if (element.namespaceURI !== ns) {
+      return false;
+    }
+
+    const tagName = element.localName;
+    const attrName = attr.name;
+
     // is it a globally safe attribute?
     if (allowed.attributes.global.indexOf(attrName) !== -1) {
       return true;
     }
+
     // are there no allowed attributes for this element?
     if (!allowed.attributes[tagName]) {
       return false;
     }
+
     // is it allowed on this element?
     // XXX the allowed list should be amendable; https://bugzil.la/922573
     if (allowed.attributes[tagName].indexOf(attrName) !== -1) {
       return true;
     }
+
     return false;
   }
 }
