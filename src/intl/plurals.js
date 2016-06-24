13f3eee1f68879614f85d2289e6dae64725e8dec
irisgau <iri.gau219@gmail.com>
2016-06-24 16:49:12 +0200

    Update Polish decimal case
    Covers polish plural case with decimals. For example, 0,3⁩ sekundy and 4,3⁩ sekundy.

--- src/intl/plurals.js
+++ src/intl/plurals.js
@@ -310,6 +310,8 @@ const pluralRules = {
     return 'other';
   },
   '13': function(n) {
+    if (n % 1 !== 0) {
+        return 'other';
     if ((isBetween((n % 10), 2, 4)) && !(isBetween((n % 100), 12, 14))) {
       return 'few';
     }
