2b7703a9ca714df79690bd2ca200c88a8baeef6f
Zibi Braniecki <gandalf@mozilla.com>
2016-08-17 13:58:37 -0700

    Fix linter error in xpcom rollup config

--- src/runtime/gecko/xpcom.rollup.config.js
+++ src/runtime/gecko/xpcom.rollup.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   preferConst: true,
-  banner: "/* vim: set ts=2 et sw=2 tw=80 filetype=javascript: */\n/* This Source Code Form is subject to the terms of the Mozilla Public\n * License, v. 2.0. If a copy of the MPL was not distributed with this\n * file, You can obtain one at http://mozilla.org/MPL/2.0/. */",
+  banner: `/* vim: set ts=2 et sw=2 tw=80 filetype=javascript: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */`,
   format: 'es',
   context: 'this'
 }
