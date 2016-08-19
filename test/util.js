42e20cd27630c062ec3d1f197e4d5b9c21f25a94
Staś Małolepszy <stas@mozilla.com>
2016-08-19 12:10:18 +0200

    isolate replaces [] now

---
+++ test/util.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Unicode bidi isolation characters
+export const FSI = '\u2068';
+export const PDI = '\u2069';
+
+// > isolate('Hello, [world].')
+// 'Hello, \u2068world\u2069.'
+export function isolate(str) {
+  return str.replace('[', FSI).replace(']', PDI);
+}
