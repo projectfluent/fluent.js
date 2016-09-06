aa78c1bfd25030717191c327d8199bd95dbf7a9d
Staś Małolepszy <stas@mozilla.com>
2016-09-06 15:29:03 +0000

    Bug 1300752 - Put perf markers in translateRootContent

--- src/lib/observer/chrome.js
+++ src/lib/observer/chrome.js
@@ -1,14 +1,24 @@
 import { LocalizationObserver } from './base';
 
+function markEnd() {
+  performance.mark('l20n: end translateRootContent');
+  performance.measure(
+    'l20n: translateRootContent',
+    'l20n: start translateRootContent',
+    'l20n: end translateRootContent'
+  );
+}
+
 export class ChromeLocalizationObserver extends LocalizationObserver {
   translateRootContent(root) {
+    performance.mark('l20n: start translateRootContent');
     const anonChildren = document.getAnonymousNodes(root);
     if (!anonChildren) {
-      return this.translateFragment(root);
+      return this.translateFragment(root).then(markEnd);
     }
 
     return Promise.all(
       [root, ...anonChildren].map(node => this.translateFragment(node))
-    );
+    ).then(markEnd);
   }
 }
