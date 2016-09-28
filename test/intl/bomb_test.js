672cd24ed444475445e9f93b6f1b38fe0be5f658
Staś Małolepszy <stas@mozilla.com>
2016-09-28 18:50:33 +0200

    bomb_test

---
+++ test/intl/bomb_test.js
@@ -0,0 +1,40 @@
+'use strict';
+
+import assert from 'assert';
+
+import { MessageContext } from '../../src/intl/context';
+import { ftl, bdi } from '../util';
+
+describe.skip('Reference bombs', function() {
+  let ctx, args, errs;
+
+  beforeEach(function() {
+    errs = [];
+  });
+
+  describe('Billion Laughs', function(){
+    before(function() {
+      ctx = new MessageContext('en-US');
+      ctx.addMessages(ftl`
+        lol0 = LOL
+        lol1 = {lol0} {lol0} {lol0} {lol0} {lol0} {lol0} {lol0} {lol0} {lol0} {lol0}
+        lol2 = {lol1} {lol1} {lol1} {lol1} {lol1} {lol1} {lol1} {lol1} {lol1} {lol1}
+        lol3 = {lol2} {lol2} {lol2} {lol2} {lol2} {lol2} {lol2} {lol2} {lol2} {lol2}
+        lol4 = {lol3} {lol3} {lol3} {lol3} {lol3} {lol3} {lol3} {lol3} {lol3} {lol3}
+        lol5 = {lol4} {lol4} {lol4} {lol4} {lol4} {lol4} {lol4} {lol4} {lol4} {lol4}
+        lol6 = {lol5} {lol5} {lol5} {lol5} {lol5} {lol5} {lol5} {lol5} {lol5} {lol5}
+        lol7 = {lol6} {lol6} {lol6} {lol6} {lol6} {lol6} {lol6} {lol6} {lol6} {lol6}
+        lol8 = {lol7} {lol7} {lol7} {lol7} {lol7} {lol7} {lol7} {lol7} {lol7} {lol7}
+        lol9 = {lol8} {lol8} {lol8} {lol8} {lol8} {lol8} {lol8} {lol8} {lol8} {lol8}
+        lolz = {lol9}
+      `);
+    });
+
+    it('does not expand all placeables', function() {
+      const msg = ctx.messages.get('lolz');
+      const val = ctx.format(msg, args, errs);
+      assert.equal(val, bdi`???`);
+      assert.equal(errs.length, 1);
+    });
+  });
+});
