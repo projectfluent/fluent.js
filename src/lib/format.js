ddf274607e482d35bf84cd899b73e21094585bfe
Staś Małolepszy <stas@mozilla.com>
2016-10-28 22:57:07 +0200

    Bug 1291695 - Improve the fallback strategy.
    Differentiate between fatal errors caused by missing translations and non-fatal
    formatting errors returned by `MessageContext`.
    
    The fallback strategy is to trigger the loading of the next language only if
    there were fatal errors in the current language.  Translations with only
    non-fatal errors are considered to be good fallback values.

--- src/lib/format.js
+++ src/lib/format.js
@@ -44,31 +44,60 @@ import { L10nError } from './errors';
  */
 export function keysFromContext(method, sanitizeArgs, ctx, keys, prev) {
   const entityErrors = [];
-  const current = {
+  const result = {
     errors: new Array(keys.length),
-    hasErrors: false
+    withoutFatal: new Array(keys.length),
+    hasFatalErrors: false,
   };
 
-  current.translations = keys.map((key, i) => {
-    if (prev && !prev.errors[i]) {
-      // Use a previously formatted good value if there were no errors
+  result.translations = keys.map((key, i) => {
+    // Use a previously formatted good value if it had no errors.
+    if (prev && !prev.errors[i] ) {
       return prev.translations[i];
     }
 
+    // Clear last entity's errors.
+    entityErrors.length = 0;
     const args = sanitizeArgs(key[1]);
     const translation = method(ctx, entityErrors, key[0], args);
-    if (entityErrors.length) {
-      current.errors[i] = entityErrors.slice();
-      entityErrors.length = 0;
-      if (!current.hasErrors) {
-        current.hasErrors = true;
-      }
+
+    // No errors still? Use this translation as fallback to the previous one
+    // which had errors.
+    if (entityErrors.length === 0) {
+      return translation;
     }
 
+    // The rest of this function handles the scenario in which the translation
+    // was formatted with errors.  Copy the errors to the result object so that
+    // the Localization can handle them (e.g. console.warn about them).
+    result.errors[i] = entityErrors.slice();
+
+    // Formatting errors are not fatal and the translations are usually still
+    // usable and can be good fallback values.  Fatal errors should signal to
+    // the Localization that another fallback should be loaded.
+    if (!entityErrors.some(isL10nError)) {
+      result.withoutFatal[i] = true;
+    } else if (!result.hasFatalErrors) {
+      result.hasFatalErrors = true;
+    }
+
+    // Use the previous translation for this `key` even if it had formatting
+    // errors.  This is usually closer the user's preferred language anyways.
+    if (prev && prev.withoutFatal[i]) {
+      // Mark this previous translation as a good potential fallback value in
+      // case of further fallbacks.
+      result.withoutFatal[i] = true;
+      return prev.translations[i];
+    }
+
+    // If no good or almost good previous translation is available, return the
+    // current translation.  In case of minor errors it's a partially
+    // formatted translation.  In the worst-case scenario it an identifier of
+    // the requested entity.
     return translation;
   });
 
-  return current;
+  return result;
 }
 
 /**
@@ -151,4 +180,16 @@ export function entityFromContext(ctx, errors, id, args) {
   }
 
   return formatted;
+}
+
+/**
+ * @private
+ *
+ * Test if an error is an instance of L10nError.
+ *
+ * @param   {Error}   error
+ * @returns {boolean}
+ */
+function isL10nError(error) {
+  return error instanceof L10nError;
 }
