7861b12518be76b78d2f1f3eca70d58c5f69ae23
Staś Małolepszy <stas@mozilla.com>
2016-11-18 21:13:55 +0100

    Support serilizing entities with no value but with traits

--- src/ftl/ast/serializer.js
+++ src/ftl/ast/serializer.js
@@ -33,15 +33,20 @@ export default {
     if (entity.comment) {
       str += `\n${this.dumpComment(entity.comment)}\n`;
     }
+
     const id = this.dumpIdentifier(entity.id);
-    const value = this.dumpPattern(entity.value);
+    str += `${id} =`;
+
+    if (entity.value) {
+      const value = this.dumpPattern(entity.value);
+      str += ` ${value}`;
+    }
 
     if (entity.traits.length) {
       const traits = this.dumpMembers(entity.traits, 2);
-      str += `${id} = ${value}\n${traits}`;
-    } else {
-      str += `${id} = ${value}`;
+      str += `\n${traits}`;
     }
+
     return str;
   },
 
