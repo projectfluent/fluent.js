bac227a0815c0168d4d292a4c84e0a201fcbb78d
Zibi Braniecki <gandalf@mozilla.com>
2017-02-02 15:19:52 -0800

    Introduce ParserStream

--- src/ftl/ast/ast.js
+++ src/ftl/ast/ast.js
@@ -18,18 +18,148 @@ class Resource extends NodeList {
   }
 }
 
-class Section extends NodeList {
-  constructor(key, body = [], comment = null) {
-    super(body, comment);
-    this.type = 'Section';
-    this.key = key;
-  }
-}
-
 class Entry extends Node {
   constructor() {
     super();
     this.type = 'Entry';
+  }
+}
+
+class Message extends Entry {
+  constructor(id, value = null, attributes = null, comment = null) {
+    super();
+    this.type = 'Entity';
+    this.id = id;
+    this.value = value;
+    this.attributes = attributes;
+    this.comment = comment;
+  }
+}
+
+class Pattern extends Node {
+  constructor(source, elements, quoted = false) {
+    super();
+    this.type = 'Pattern';
+    this.elements = elements
+    this.quoted = quoted;
+  }
+}
+
+class Expression extends Node {
+  constructor() {
+    super();
+    this.type = 'Expression';
+  }
+}
+
+class PatternExpression extends Expression {
+  constructor(pattern) {
+    super();
+    this.type = 'PatternExpression';
+    this.pattern = pattern;
+  }
+}
+
+class StringExpression extends Expression {
+  constructor(string) {
+    super();
+    this.type = 'StringExpression';
+    this.string = string;
+  }
+}
+
+class NumberExpression extends Expression {
+  constructor(number) {
+    super();
+    this.type = 'NumberExpression';
+    this.number = number;
+  }
+}
+
+class MessageReference extends Expression {
+  constructor(id) {
+    super();
+    this.type = 'MessageReference';
+    this.id = id;
+  }
+}
+
+class ExternalArgument extends Expression {
+  constructor(id) {
+    super();
+    this.type = 'ExternalArgument';
+    this.id = id;
+  }
+}
+
+class SelectExpression extends Expression {
+  constructor(exp, variants = null) {
+    super();
+    this.type = 'SelectExpression';
+    this.exp = exp;
+    this.variants = variants;
+  }
+}
+
+class AttributeExpression extends Expression {
+  constructor(id, name) {
+    super();
+    this.type = 'AttributeExpression';
+    this.id = id;
+    this.name = name;
+  }
+}
+
+class VariantExpression extends Expression {
+  constructor(id, key) {
+    super();
+    this.type = 'VariantkExpression';
+    this.id = id;
+    this.key = key;
+  }
+}
+
+class CallExpression extends Expression {
+  constructor(callee, args) {
+    super();
+    this.type = 'CallExpression';
+    this.callee = callee;
+    this.args = args;
+  }
+}
+
+class ExpressionExpression extends Expression {
+  constructor(callee, exp) {
+    super();
+    this.type = 'ExpressionExpression';
+    this.exp = exp;
+  }
+}
+
+class Attribute extends Node {
+  constructor(id, value) {
+    super();
+    this.type = 'Attribute';
+    this.id = id;
+    this.value = value;
+  }
+}
+
+class Variant extends Node {
+  constructor(key, value, def = false) {
+    super();
+    this.type = 'Variant';
+    this.key = key;
+    this.value = value;
+    this.default = def;
+  }
+}
+
+class NamedArgument extends Node {
+  constructor(name, val) {
+    super();
+    this.name = name;
+    this.val = val;
   }
 }
 
@@ -41,108 +171,28 @@ class Identifier extends Node {
   }
 }
 
-class Pattern extends Node {
-  constructor(source, elements, quoted = false) {
-    super();
-    this.type = 'Pattern';
-    this.source = source;
-    this.elements = elements;
-    this.quoted = quoted;
-  }
-}
-
-class Member extends Node {
-  constructor(key, value, def = false) {
-    super();
-    this.type = 'Member';
-    this.key = key;
-    this.value = value;
-    this.default = def;
-  }
-}
-
-class Entity extends Entry {
-  constructor(id, value = null, traits = [], comment = null) {
-    super();
-    this.type = 'Entity';
-    this.id = id;
-    this.value = value;
-    this.traits = traits;
-    this.comment = comment;
-  }
-}
-
-class Placeable extends Node {
-  constructor(expressions) {
-    super();
-    this.type = 'Placeable';
-    this.expressions = expressions;
-  }
-}
-
-class SelectExpression extends Node {
-  constructor(expression, variants = null) {
-    super();
-    this.type = 'SelectExpression';
-    this.expression = expression;
-    this.variants = variants;
-  }
-}
-
-class MemberExpression extends Node {
-  constructor(obj, keyword) {
-    super();
-    this.type = 'MemberExpression';
-    this.object = obj;
-    this.keyword = keyword;
-  }
-}
-
-class CallExpression extends Node {
-  constructor(callee, args) {
-    super();
-    this.type = 'CallExpression';
-    this.callee = callee;
-    this.args = args;
-  }
-}
-
-class ExternalArgument extends Node {
-  constructor(name) {
-    super();
-    this.type = 'ExternalArgument';
-    this.name = name;
-  }
-}
-
-class KeyValueArg extends Node {
-  constructor(name, value) {
-    super();
-    this.type = 'KeyValueArg';
-    this.name = name;
-    this.value = value;
-  }
-}
-
-class EntityReference extends Identifier {
-  constructor(name) {
-    super(name);
-    this.type = 'EntityReference';
-  }
-}
-
-class FunctionReference extends Identifier {
-  constructor(name) {
-    super(name);
-    this.type = 'FunctionReference';
-  }
-}
-
 class Keyword extends Identifier {
   constructor(name, namespace = null) {
     super(name);
     this.type = 'Keyword';
     this.namespace = namespace;
+  }
+}
+
+class Comment extends Node {
+  constructor(content) {
+    super();
+    this.type = 'Comment';
+    this.content = content;
+  }
+}
+
+class Section extends Node {
+  constructor(key, comment = null) {
+    super();
+    this.type = 'Section';
+    this.key = key;
+    this.comment = comment;
   }
 }
 
@@ -154,19 +204,10 @@ class Number extends Node {
   }
 }
 
-class TextElement extends Node {
-  constructor(value) {
-    super();
-    this.type = 'TextElement';
-    this.value = value;
-  }
-}
-
-class Comment extends Node {
-  constructor(content) {
-    super();
-    this.type = 'Comment';
-    this.content = content;
+class Function extends Identifier {
+  constructor(name) {
+    super(name);
+    this.type = 'Function';
   }
 }
 
@@ -179,24 +220,28 @@ class JunkEntry extends Entry {
 }
 
 export default {
-  Node,
-  Pattern,
-  Member,
-  Identifier,
-  Entity,
-  Section,
   Resource,
-  Placeable,
-  SelectExpression,
-  MemberExpression,
-  CallExpression,
+  Message,
+  Entry,
+  Pattern,
+  Expression,
+  PatternExpression,
+  StringExpression,
+  NumberExpression,
+  MessageReference,
   ExternalArgument,
-  KeyValueArg,
-  Number,
-  EntityReference,
-  FunctionReference,
+  SelectExpression,
+  AttributeExpression,
+  VariantExpression,
+  CallExpression,
+  ExpressionExpression,
+  Attribute,
+  Variant,
+  Identifier,
   Keyword,
-  TextElement,
   Comment,
-  JunkEntry
+  Section,
+  Number,
+  Function,
+  JunkEntry,
 };
---
+++ src/ftl/ast/iter.js
@@ -0,0 +1,120 @@
+export class ParserStream {
+  constructor(string) {
+    this.iter = string[Symbol.iterator]();
+    this.buf = [];
+    this.peekIndex = 0;
+    this.index = 0;
+
+    this.ch = null;
+
+    this.iterEnd = false;
+    this.peekEnd = false;
+
+    this.ch = this.iter.next().value;
+  }
+
+  next() {
+    if (this.iterEnd) {
+      return undefined;
+    }
+
+    if (this.buf.length === 0) {
+      this.ch = this.iter.next().value;
+    } else {
+      this.ch = this.buf.shift();
+    }
+
+    this.index++;
+
+    if (this.ch === undefined) {
+      this.iterEnd = true;
+      this.peekEnd = true;
+    }
+
+    this.peekIndex = this.index;
+
+    return this.ch;
+  }
+
+  current() {
+    return this.ch;
+  }
+
+  currentIs(ch) {
+    return this.ch === ch;
+  }
+
+  currentPeek() {
+    if (this.peekEnd) {
+      return undefined;
+    }
+
+    let diff = this.peekIndex - this.index;
+
+    if (diff === 0) {
+      return this.ch;
+    }
+    return this.buf[diff - 1];
+  }
+
+  currentPeekIs(ch) {
+    return this.currentPeek() === ch;
+  }
+
+  peek() {
+    if (this.peekEnd) {
+      return undefined;
+    }
+
+    this.peekIndex += 1;
+
+    let diff = this.peekIndex - this.index;
+
+    if (diff > this.buf.length) {
+      let ch = this.iter.next().value;
+      if (ch !== undefined) {
+        this.buf.push(ch);
+      } else {
+        this.peekEnd = true;
+        return undefined;
+      }
+    }
+
+    return this.buf[diff - 1];
+  }
+
+  getIndex() {
+    return this.index;
+  }
+
+  getPeekIndex() {
+    return this.peekIndex;
+  }
+
+  peekCharIs(ch) {
+    if (this.peekEnd) {
+      return false;
+    }
+
+    let ret = this.peek();
+
+    this.peekIndex -= 1;
+
+    return ret === ch;
+  }
+
+  resetPeek() {
+    this.peekIndex = this.index;
+    this.peekEnd = this.iterEnd;
+  }
+
+  skipToPeek() {
+    let diff = this.peekIndex - this.index;
+
+    for (let i = 0; i < diff; i++) {
+      this.ch = this.buf.shift();
+    }
+
+    this.index = this.peekIndex;
+  }
+}
