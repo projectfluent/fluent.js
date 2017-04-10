57a7dbe9e58c8dc995afd2f9cf5167668291e720
Staś Małolepszy <stas@mozilla.com>
2017-04-10 16:18:52 +0200

    Structure tests for the runtime parser

---
+++ fluent/test/parser_structure_test.js
@@ -0,0 +1,40 @@
+import assert from 'assert';
+import { join } from 'path';
+import { readdir } from 'fs';
+import { readfile } from '../../fluent-syntax/test/util';
+
+import parse from '../src/parser';
+
+const ftlFixtures = join(
+  __dirname, '..', '..', 'fluent-syntax', 'test', 'fixtures_structure'
+);
+const jsonFixtures = join(__dirname, 'fixtures_structure');
+
+readdir(ftlFixtures, function(err, filenames) {
+  if (err) {
+    throw err;
+  }
+
+  const ftlnames = filenames.filter(
+    filename => filename.endsWith('.ftl')
+  );
+
+  suite('Structure tests', function() {
+    for (const ftlfilename of ftlnames) {
+      const jsonfilename = ftlfilename.replace(/ftl$/, 'json');
+      const ftlpath = join(ftlFixtures, ftlfilename);
+      const jsonpath = join(jsonFixtures, jsonfilename);
+      test(ftlfilename, function() {
+        return Promise.all(
+          [ftlpath, jsonpath].map(readfile)
+        ).then(([ftl, expected]) => {
+          const [entries] = parse(ftl);
+          assert.deepEqual(
+            entries, JSON.parse(expected),
+            'Actual Annotations don\'t match the expected ones'
+          );
+        });
+      });
+    }
+  });
+});
