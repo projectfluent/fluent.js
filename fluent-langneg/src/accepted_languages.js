51818649184f9e12c663f43ac6aae8443dbc5ec4
Zibi Braniecki <zibi@braniecki.net>
2017-05-18 15:23:13 -0700

    Add Headers parsing function (#38)
    * Add Headers parsing function
    
    * Apply feedback
    
    * Updated to feedback

---
+++ fluent-langneg/src/accepted_languages.js
@@ -0,0 +1,8 @@
+
+export default function acceptedLanguages(string = '') {
+  if (typeof string !== 'string') {
+    throw new TypeError('Argument must be a string');
+  }
+  const tokens = string.split(',').map(t => t.trim());
+  return tokens.filter(t => t !== '').map(t => t.split(';')[0]);
+}
---
+++ fluent-langneg/test/headers_test.js
@@ -0,0 +1,45 @@
+import assert from 'assert';
+import acceptedLanguages from '../src/accepted_languages';
+
+suite('parse headers', () => {
+  test('without quality values', () => {
+    assert.deepStrictEqual(
+      acceptedLanguages('en-US, fr, pl'), [
+        'en-US',
+        'fr',
+        'pl'
+      ]
+    );
+    assert.deepStrictEqual(
+      acceptedLanguages('sr-Latn'), [
+        'sr-Latn'
+      ]
+    );
+  });
+
+  test('with quality values', () => {
+    assert.deepStrictEqual(
+      acceptedLanguages('fr-CH, fr;q=0.9, en;q=0.8, de;q=0.7, *;q=0.5'), [
+        'fr-CH',
+        'fr',
+        'en',
+        'de',
+        '*'
+      ]
+    );
+  });
+
+  test('edge cases', () => {
+    const args = [
+      null,
+      NaN,
+      Infinity,
+      [],
+      {}
+    ];
+
+    args.forEach(arg => {
+      assert.throws(acceptedLanguages.bind(null, arg), TypeError);
+    });
+  });
+});
