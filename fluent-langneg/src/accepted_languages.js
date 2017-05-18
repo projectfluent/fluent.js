6423f7dc4af33e82e44ddc76e25ff3fcd70ad3b4
Zibi Braniecki <gandalf@mozilla.com>
2017-05-18 15:18:49 -0700

    Updated to feedback

--- fluent-langneg/src/accepted_languages.js
+++ fluent-langneg/src/accepted_languages.js
@@ -1,10 +1,8 @@
 
-export default function aceptedLanguages(string = '') {
+export default function acceptedLanguages(string = '') {
   if (typeof string !== 'string') {
     throw new TypeError('Argument must be a string');
   }
   const tokens = string.split(',').map(t => t.trim());
-  return tokens.filter(t => t !== '').map(t => {
-    return t.split(';')[0];
-  });
+  return tokens.filter(t => t !== '').map(t => t.split(';')[0]);
 }
