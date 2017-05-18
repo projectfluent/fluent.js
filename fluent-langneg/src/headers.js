7feb8ee6b993101f4f749f5d2b43b19000f72a28
Zibi Braniecki <gandalf@mozilla.com>
2017-05-18 10:47:05 -0700

    Add Headers parsing function

---
+++ fluent-langneg/src/headers.js
@@ -0,0 +1,8 @@
+
+export default function parseAcceptedLanguagesHeaderIntoArray(string = '') {
+  if (typeof string !== 'string') throw new TypeError('Argument must be a string');
+  const tokens = string.split(',').map(t => t.trim());
+  return tokens.filter(t => t !== '').map(t => {
+    return t.split(';')[0];
+  }); 
+}
