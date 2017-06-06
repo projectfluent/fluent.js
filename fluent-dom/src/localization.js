914e63745dfaec0915119b5d59f3dd0789b30329
Zibi Braniecki <gandalf@mozilla.com>
2017-06-06 12:24:53 +0200

    Fix jsdoc

--- fluent-dom/src/localization.js
+++ fluent-dom/src/localization.js
@@ -22,7 +22,7 @@ export default class Localization {
   constructor(resIds, generateMessages) {
     this.resIds = resIds;
     this.generateMessages = generateMessages;
-    this.ctxs = this.generateMessages(this.id, this.resIds);
+    this.ctxs = this.generateMessages(this.resIds);
   }
 
   /**
@@ -41,7 +41,8 @@ export default class Localization {
     const translations = [];
     for (let ctx of this.ctxs) {
       if (typeof ctx.then === 'function') {
-        ctx = await ctx;
+        //XXX: Remove once jsdoc supports `await ctx`
+        ctx = await ctx.then();
       }
       const errors = keysFromContext(method, ctx, keys, translations);
       if (!errors) {
@@ -132,7 +133,7 @@ export default class Localization {
   }
 
   onLanguageChange() {
-    this.ctxs = this.generateMessages(this.id, this.resIds);
+    this.ctxs = this.generateMessages(this.resIds);
   }
 }
 
