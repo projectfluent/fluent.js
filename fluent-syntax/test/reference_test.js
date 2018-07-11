b32a35dfc5e4d2b3031caea9a97a67195c6e5a87
Staś Małolepszy <stas@mozilla.com>
2018-07-11 13:43:21 +0200

    Unskip most of the reference tests

--- fluent-syntax/test/reference_test.js
+++ fluent-syntax/test/reference_test.js
@@ -15,10 +15,40 @@ readdir(fixtures, function(err, filenames) {
     filename => filename.endsWith(".ftl")
   );
 
-  suite.skip("Reference tests", function() {
+  // XXX This suite is not ideal. We ignore Junk and skip a number of fixtures.
+  suite("Reference tests", function() {
     const parser = new FluentParser({withSpans: false});
 
+    // The following fixtures produce different ASTs in the tooling parser than
+    // in the reference parser. Skip them for now.
+    const skips = [
+      // Call arguments edge-cases.
+      "call_expressions.ftl",
+
+      // isPeekNextLineComment gives a false positive when the next line doesn't
+      // start with # but is indented by at least (n + 1) spaces, where n is the
+      // current comment level.
+      "mixed_entries.ftl",
+
+      // The tooling parser rejects variant keys which contain leading whitespace.
+      // There's even a behavior fixture for this; it must have been a
+      // deliberate decision.
+      "select_expressions.ftl",
+
+      // Broken Attributes break the entire Entry right now.
+      // https://github.com/projectfluent/fluent.js/issues/237
+      "leading_dots.ftl",
+      "variant_lists.ftl"
+    ];
+
     for (const filename of ftlnames) {
+
+      // Skip the known AST incompatibilities.
+      if (skips.includes(filename)) {
+        test.skip(filename);
+        continue;
+      }
+
       const ftlpath = join(fixtures, filename);
       const astpath = ftlpath.replace(/ftl$/, "json");
 
@@ -29,6 +59,8 @@ readdir(fixtures, function(err, filenames) {
         const ref = JSON.parse(expected)
         const ast = parser.parse(ftl);
 
+        // Ignore Junk which is parsed differently by the tooling parser, and
+        // which doesn't carry spans nor annotations in the reference parser.
         ref.body = ref.body.filter(entry => entry.type !== "Junk");
         ast.body = ast.body.filter(entry => entry.type !== "Junk");
 
