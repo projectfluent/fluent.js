ac828c5f0c681696d92143dc52916fa4f77b558c
Axel Hecht <axel@pike.org>
2018-08-09 17:47:44 +0200

    Add node 10 and 12 to test matrix, disable pl pluralrules test.
    Also, I'm making node 8 not specify the subrevision, there are
    newer versions of it by now, and we're not specific on 10 nor 12.
    
    The test for Polish plural forms requires that Intl.PluralRules
    exists for Polish, and the standard node distributions don't
    ship with ICU. Let's rely on the upstream tests.

--- fluent-intl-polyfill/test/plural_rules_test.js
+++ fluent-intl-polyfill/test/plural_rules_test.js
@@ -28,16 +28,6 @@ suite('Plural rules', function() {
     assert.strictEqual(pr.select(22), 'other');
   });
 
-  test('known language: pl', function() {
-    const pr = new Intl.PluralRules('pl');
-    assert.strictEqual(pr.select(0), 'many');
-    assert.strictEqual(pr.select(1), 'one');
-    assert.strictEqual(pr.select(3), 'few');
-    assert.strictEqual(pr.select(5), 'many');
-    assert.strictEqual(pr.select(12), 'many');
-    assert.strictEqual(pr.select(22), 'few');
-  });
-
   test('unknown languages uses en-US', function() {
     const pr = new Intl.PluralRules('xxx');
     assert.strictEqual(pr.select(0), 'other');
