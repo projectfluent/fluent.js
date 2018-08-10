8af740ada7fb8131bb54bcda06f5eb3eab7eda89
Staś Małolepszy <stas@mozilla.com>
2018-08-10 14:15:34 +0200

    Also call createParseMarkup in LocalizationProvider
    This allows developers to catch the problem early.

--- fluent-react/src/provider.js
+++ fluent-react/src/provider.js
@@ -1,6 +1,7 @@
 import { Component, Children } from "react";
 import PropTypes from "prop-types";
 import ReactLocalization, { isReactLocalization} from "./localization";
+import createParseMarkup from "./markup";
 
 /*
  * The Provider component for the `ReactLocalization` class.
@@ -23,7 +24,7 @@ import ReactLocalization, { isReactLocalization} from "./localization";
 export default class LocalizationProvider extends Component {
   constructor(props) {
     super(props);
-    const { messages } = props;
+    const {messages, parseMarkup} = props;
 
     if (messages === undefined) {
       throw new Error("LocalizationProvider must receive the messages prop.");
@@ -34,12 +35,13 @@ export default class LocalizationProvider extends Component {
     }
 
     this.l10n = new ReactLocalization(messages);
+    this.parseMarkup = parseMarkup || createParseMarkup();
   }
 
   getChildContext() {
     return {
       l10n: this.l10n,
-      parseMarkup: this.props.parseMarkup,
+      parseMarkup: this.parseMarkup,
     };
   }
 
