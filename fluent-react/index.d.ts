61ae37f96255ddb1646ad91a1078ca194a65e042
Mark Weaver <mark@blushingpenguin.com>
2018-09-04 13:32:27 +0000

    update to latest fluent interface

--- fluent-react/index.d.ts
+++ fluent-react/index.d.ts
@@ -1,54 +1,89 @@
+// vim: set ts=2 sw=2
 import * as fluent from 'fluent';
 import * as React from 'react';
 
-export declare interface LocalizedProps
-{
-    getString(id: string, args?: object, fallback?: string): string;
+export declare interface LocalizedProps {
+  getString(id: string, args?: object, fallback?: string): string;
 }
 
 export declare type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
 
 export declare function withLocalization<P extends LocalizedProps>(
-    WrappedComponent: React.ComponentType<P>): React.ComponentType<Omit<P, keyof LocalizedProps>>;
+  WrappedComponent: React.ComponentType<P>): React.ComponentType<Omit<P, keyof LocalizedProps>>;
 
-export declare function isReactLocalization(props: object, propName: string) : Error | null;
+export declare function isReactLocalization(props: object, propName: string): Error | null;
 
 export declare interface LocalizationProviderProps {
-    messages: Iterable<fluent.MessageContext>,
-    children?: React.ReactNode
+  bundles: Iterable<fluent.FluentBundle>
+  parseMarkup?: (str: string) => NodeListOf<Node & ChildNode>
+  children?: React.ReactNode
 }
 
+/*
+ * `ReactLocalization` handles translation formatting and fallback.
+ *
+ * The current negotiated fallback chain of languages is stored in the
+ * `ReactLocalization` instance in form of an iterable of `FluentBundle`
+ * instances.  This iterable is used to find the best existing translation for
+ * a given identifier.
+ *
+ * `Localized` components must subscribe to the changes of the
+ * `ReactLocalization`'s fallback chain.  When the fallback chain changes (the
+ * `bundles` iterable is set anew), all subscribed compontent must relocalize.
+ *
+ * The `ReactLocalization` class instances are exposed to `Localized` elements
+ * via the `LocalizationProvider` component.
+ */
 export declare class ReactLocalization {
-    constructor(messages: Iterable<string>);
-    public subscribe(component: React.Component): void;
-    public unsubscribe(component: React.Component): void;
-    public setMessages(messages: Iterable<string>): void;
-    public getMessageContext(id: string): fluent.MessageContext;
-    public formatCompound(mcx: fluent.MessageContext, msg: string, args?: object): {
-        value: string,
-        attrs?: object
-    };
-    public getString(id: string, args?: object, fallback?: string): string;
+  constructor(messages: Iterable<string>);
+
+  /*
+   * Subscribe a `Localized` component to changes of `bundles`.
+   */
+  public subscribe(component: React.Component): void;
+
+  /*
+   * Unsubscribe a `Localized` component from `bundles` changes.
+   */
+  public unsubscribe(component: React.Component): void;
+
+  /*
+   * Set a new `bundles` iterable and trigger the retranslation.
+   */
+  public setBundles(messages: Iterable<fluent.FluentBundle>): void;
+  
+  public getBundle(id: string): fluent.FluentBundle;
+  
+  public formatCompound(mcx: fluent.FluentBundle, msg: string, args?: object): {
+      value: string,
+      attrs?: object
+  };
+  
+  /*
+   * Find a translation by `id` and format it to a string using `args`.
+   */
+  public getString(id: string, args?: object, fallback?: string): string;
 }
 
 export declare interface ChildContextType {
     l10n: ReactLocalization
+    parseMarkup?: (str: string) => NodeListOf<Node & ChildNode>
 }
 
 export declare class LocalizationProvider extends React.Component<LocalizationProviderProps> {
     public static childContextTypes: ChildContextType;
     constructor(props: LocalizationProviderProps);
-    public getChildContext(): ReactLocalization;
+    public getChildContext(): ChildContextType;
     public componentWillReceiveProps(next: LocalizationProviderProps): void;
-    public render() : React.ReactNode;
+    public render(): React.ReactNode;
 }
 
-export declare interface LocalizedProperties {
+export declare interface LocalizedComponentProps {
     id: string;
     children?: React.ReactNode;
 }
 
-export declare class Localized extends React.Component<any> {
+export declare class Localized extends React.Component<LocalizedComponentProps> {
     public componentDidMount(): void;
     public componentWillUnmount(): void;
 
--- fluent/index.d.ts
+++ fluent/index.d.ts
@@ -1,19 +1,151 @@
 import * as React from 'react';
 
-interface MessageContextOptions {
-    functions?: object
-    useIsolating: boolean
-}
-interface Message {
-    id: string
-    message: string
+/**
+ * Fluent Resource is a structure storing a map
+ * of localization entries.
+ */
+export class FluentResource extends Map {
+    constructor(entries: object, errors: Error[]);
+    static fromString(source: string): FluentResource;
+}  
+  
+interface FluentBundleOptions {
+  functions?: object
+  useIsolating: boolean
+  transform: (s: string) => string
 }
 
-export class MessageContext {
-    constructor(locales: string|string[], options?: MessageContextOptions);
-    public getMessages(): Iterable<Message>; // TODO get messages() --- an accessor cannot be declared in ambient context
-    public hasMessage(id: string): boolean;
-    public getMessage(id: string): string;
-    public addMessages(messages: string): void;
-    public format(message: object|string, args: object|undefined, errors: string[]): string;
+interface Message {
+  id: string
+  message: string
+}
+
+export class FluentBundle {
+  /**
+   * Create an instance of `FluentBundle`.
+   *
+   * The `locales` argument is used to instantiate `Intl` formatters used by
+   * translations.  The `options` object can be used to configure the context.
+   *
+   * Examples:
+   *
+   *     const bundle = new FluentBundle(locales);
+   *
+   *     const bundle = new FluentBundle(locales, { useIsolating: false });
+   *
+   *     const bundle = new FluentBundle(locales, {
+   *       useIsolating: true,
+   *       functions: {
+   *         NODE_ENV: () => process.env.NODE_ENV
+   *       }
+   *     });
+   *
+   * Available options:
+   *
+   *   - `functions` - an object of additional functions available to
+   *                   translations as builtins.
+   *
+   *   - `useIsolating` - boolean specifying whether to use Unicode isolation
+   *                    marks (FSI, PDI) for bidi interpolations.
+   *
+   *   - `transform` - a function used to transform string parts of patterns.
+   *
+   * @param   {string|Array<string>} locales - Locale or locales of the context
+   * @param   {Object} [options]
+   * @returns {FluentBundle}
+   */
+  constructor(locales: string|string[], options?: FluentBundleOptions);
+
+  public messages(): Iterable<Message>;
+
+  /*
+   * Check if a message is present in the context.
+   *
+   * @param {string} id - The identifier of the message to check.
+   * @returns {bool}
+   */
+  public hasMessage(id: string): boolean;
+
+  /*
+   * Return the internal representation of a message.
+   *
+   * The internal representation should only be used as an argument to
+   * `FluentBundle.format`.
+   *
+   * @param {string} id - The identifier of the message to check.
+   * @returns {Any}
+   */
+  public getMessage(id: string): string;
+
+  /**
+   * Add a translation resource to the context.
+   *
+   * The translation resource must use the Fluent syntax.  It will be parsed by
+   * the context and each translation unit (message) will be available in the
+   * context by its identifier.
+   *
+   *     bundle.addMessages('foo = Foo');
+   *     bundle.getMessage('foo');
+   *
+   *     // Returns a raw representation of the 'foo' message.
+   *
+   * Parsed entities should be formatted with the `format` method in case they
+   * contain logic (references, select expressions etc.).
+   *
+   * @param   {string} source - Text resource with translations.
+   * @returns {Array<Error>}
+   */
+  public addMessages(messages: string): void;
+
+  /**
+   * Add a translation resource to the context.
+   *
+   * The translation resource must be a proper FluentResource
+   * parsed by `FluentBundle.parseResource`.
+   *
+   *     let res = FluentBundle.parseResource("foo = Foo");
+   *     bundle.addResource(res);
+   *     bundle.getMessage('foo');
+   *
+   *     // Returns a raw representation of the 'foo' message.
+   *
+   * Parsed entities should be formatted with the `format` method in case they
+   * contain logic (references, select expressions etc.).
+   *
+   * @param   {FluentResource} res - FluentResource object.
+   * @returns {Array<Error>}
+   */
+  public addResource(res: FluentResource): void
+
+  /**
+   * Format a message to a string or null.
+   *
+   * Format a raw `message` from the context into a string (or a null if it has
+   * a null value).  `args` will be used to resolve references to variables
+   * passed as arguments to the translation.
+   *
+   * In case of errors `format` will try to salvage as much of the translation
+   * as possible and will still return a string.  For performance reasons, the
+   * encountered errors are not returned but instead are appended to the
+   * `errors` array passed as the third argument.
+   *
+   *     const errors = [];
+   *     bundle.addMessages('hello = Hello, { $name }!');
+   *     const hello = bundle.getMessage('hello');
+   *     bundle.format(hello, { name: 'Jane' }, errors);
+   *
+   *     // Returns 'Hello, Jane!' and `errors` is empty.
+   *
+   *     bundle.format(hello, undefined, errors);
+   *
+   *     // Returns 'Hello, name!' and `errors` is now:
+   *
+   *     [<ReferenceError: Unknown variable: name>]
+   *
+   * @param   {Object | string}    message
+   * @param   {Object | undefined} args
+   * @param   {Array}              errors
+   * @returns {?string}
+   */
+  public format(message: object|string, args: object|undefined, errors: string[]): string;
 }
