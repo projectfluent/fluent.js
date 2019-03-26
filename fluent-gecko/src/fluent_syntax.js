65e5f1d1ae43546fc4cf274daf265c49be867515
Staś Małolepszy <stas@mozilla.com>
2019-03-26 17:25:50 +0100

    Export Visitor and Transformer from FluentSyntax.jsm

--- fluent-gecko/src/fluent_syntax.js
+++ fluent-gecko/src/fluent_syntax.js
@@ -1,15 +1,18 @@
 /* eslint object-shorthand: "off",
-          no-unused-vars: "off",
           no-redeclare: "off",
           comma-dangle: "off",
           no-labels: "off" */
 
-import FluentParser from "../../fluent-syntax/src/parser";
-import FluentSerializer from "../../fluent-syntax/src/serializer";
+import {FluentParser} from "../../fluent-syntax/src/parser";
+import {FluentSerializer} from "../../fluent-syntax/src/serializer";
 import * as ast from "../../fluent-syntax/src/ast";
+import * as visitor from "../../fluent-syntax/src/visitor";
 
 this.EXPORTED_SYMBOLS = [
-  "FluentParser",
-  "FluentSerializer",
+  ...Object.keys({
+    FluentParser,
+    FluentSerializer,
+  }),
   ...Object.keys(ast),
+  ...Object.keys(visitor),
 ];
