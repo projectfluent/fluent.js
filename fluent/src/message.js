d6fe9691ef642de676acbcf3880b73d83302b2f9
Staś Małolepszy <stas@mozilla.com>
2019-04-29 18:15:24 +0200

    Document FluentMessage

--- fluent/src/message.js
+++ fluent/src/message.js
@@ -5,13 +5,20 @@ export default
 class FluentMessage {
   constructor(id, value, attributes) {
     this.id = id;
+
+    // Reflection fields allow consumers to decide which patterns to format.
     this.hasValue = value !== null;
     this.attributeNames = Object.keys(attributes);
 
+    // Semi-private fields for storing the runtime AST of the message.
+    // Should not be accessed directly.
     this._value = value;
     this._attributes = attributes;
   }
 
+  // Resolve the value of the message into a FluentType. Used by
+  // FluentBundle.{format,formatValue} and when resolving references to
+  // messages and terms from other patterns.
   resolveValue(scope) {
     // Handle messages with null values.
     if (this._value === null) {
@@ -21,6 +28,9 @@ class FluentMessage {
     return Type(scope, this._value);
   }
 
+  // Resolve the attribute of the message into a FluentType. Used by
+  // FluentBundle.formatAttribute and when resolving references to
+  // attributes of messages and terms from other patterns.
   resolveAttribute(scope, name) {
     let attribute = this._attributes[name];
     if (attribute === undefined){
