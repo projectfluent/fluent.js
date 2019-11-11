2181ce440522d5a04972db6095e5adbb429bf5cc
Anand Thakker <github@anandthakker.net>
2019-11-11 09:18:37 -0500

    [Desmos Fork] Target IE11 in compat build
    Note: see also https://github.com/projectfluent/fluent.js/issues/86

--- compat_config.js
+++ compat_config.js
@@ -1,11 +1,15 @@
 import babel from 'rollup-plugin-babel';
 import babelConfig from './babel_config';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
 
 export default {
   output: {
-    format: 'umd'
+    format: 'amd'
   },
   plugins: [
     babel(babelConfig),
+    resolve(),
+    commonjs()
   ],
 };
