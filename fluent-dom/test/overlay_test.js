a45b59b19359f5d2c6c2943aa177da9d23bd0ebc
Zibi Braniecki <zibi@braniecki.net>
2019-12-03 15:36:51 -0800

    Exclude <title> from HTML sanitization

---
+++ fluent-dom/test/overlay_test.js
@@ -0,0 +1,19 @@
+import assert from 'assert';
+import translateElement from '../src/overlay';
+import {elem} from './index';
+
+suite('Applying translations', function() {
+  test('Skipping sanitization for the title element', function() {
+    const element = elem('title')``;
+    const translation = {
+      value: '<input type="text"/> - HTML: Input Element',
+      attributes: null
+    };
+
+    translateElement(element, translation);
+    assert.strictEqual(
+      element.innerHTML,
+      '&lt;input type="text"/&gt; - HTML: Input Element'
+    );
+  });
+});
