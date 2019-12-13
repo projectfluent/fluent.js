d02a9c5455a20b8e25de11fd3f7de37c5282ce69
Gregor Weber <gregorwbr@gmail.com>
2019-12-13 12:30:29 +0100

    Correctly initialize context

--- fluent-react/src/context.js
+++ fluent-react/src/context.js
@@ -1,4 +1,7 @@
 import { createContext } from "react";
 import ReactLocalization from "./localization";
 
-export default createContext(new ReactLocalization([]));
+export default createContext({
+  l10n: new ReactLocalization([]),
+  parseMarkup: null
+});
