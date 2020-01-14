9d49e48615c853f926243498776bb352577b19cd
Staś Małolepszy <stas@mozilla.com>
2020-01-14 15:18:48 +0100

    Simplify getString in withLocalization

--- fluent-react/src/with_localization.js
+++ fluent-react/src/with_localization.js
@@ -6,13 +6,9 @@ export default function withLocalization(Inner) {
     const l10n = useContext(FluentContext);
     return createElement(
       Inner,
-      // getString needs to be re-bound on updates to trigger a re-render
       {
-        getString: (id, args, fallback) => (
-          l10n
-            ? l10n.getString(id, args, fallback)
-            : fallback || id
-        ),
+        // getString needs to be re-bound to trigger a re-render of Inner
+        getString: (id, args, fallback) => l10n.getString(id, args, fallback),
         ...props
       },
     );
