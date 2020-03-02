f1e8678c8fd09ca07f0c8b1af027cff1d0632729
Staś Małolepszy <stas@mozilla.com>
2020-03-02 14:56:55 +0100

    Updates

---
+++ dedent/assets/js/search.js
@@ -0,0 +1,3 @@
+var typedoc = typedoc || {};
+            typedoc.search = typedoc.search || {};
+            typedoc.search.data = {"kinds":{"64":"Function"},"rows":[{"id":0,"kind":64,"name":"ftl","url":"globals.html#ftl","classes":"tsd-kind-function"}]};
---
+++ sequence/assets/js/search.js
@@ -0,0 +1,3 @@
+var typedoc = typedoc || {};
+            typedoc.search = typedoc.search || {};
+            typedoc.search.data = {"kinds":{"64":"Function"},"rows":[{"id":0,"kind":64,"name":"mapBundleSync","url":"globals.html#mapbundlesync","classes":"tsd-kind-function"},{"id":1,"kind":64,"name":"mapBundleAsync","url":"globals.html#mapbundleasync","classes":"tsd-kind-function"}]};
