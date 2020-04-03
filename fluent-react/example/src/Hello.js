79024fca8257570c37ade020349a1699b4ac854b
Staś Małolepszy <stas@mozilla.com>
2020-04-03 18:54:24 +0200

    Rewrite Hello.ts to use hooks

--- fluent-react/example/src/Hello.js
+++ fluent-react/example/src/Hello.js
@@ -1,42 +1,29 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Localized } from "@fluent/react";
 
-export class Hello extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      userName: ''
-    };
-  }
-
-  handleNameChange(userName) {
-    this.setState({ userName });
-  }
-
-  render() {
-    const { userName } = this.state;
+export function Hello() {
+    let [userName, setUserName] = useState("");
 
     return (
-      <div>
-        { userName ?
-            <Localized id="hello" vars={{userName}}>
-              <h1>{'Hello, { $userName }!'}</h1>
-            </Localized>
-          :
-            <Localized id="hello-no-name">
-              <h1>Hello, stranger!</h1>
-            </Localized>
-        }
+        <div>
+            {userName ?
+                <Localized id="hello" vars={{ userName }}>
+                    <h1>{'Hello, { $userName }!'}</h1>
+                </Localized>
+                :
+                <Localized id="hello-no-name">
+                    <h1>Hello, stranger!</h1>
+                </Localized>
+            }
 
-        <Localized id="type-name" attrs={{placeholder: true}}>
-          <input
-            type="text"
-            placeholder="Type your name"
-            onChange={evt => this.handleNameChange(evt.target.value)}
-            value={userName}
-          />
-        </Localized>
-      </div>
+            <Localized id="type-name" attrs={{ placeholder: true }}>
+                <input
+                    type="text"
+                    placeholder="Type your name"
+                    onChange={evt => setUserName(evt.target.value)}
+                    value={userName}
+                />
+            </Localized>
+        </div>
     );
-  }
 }
