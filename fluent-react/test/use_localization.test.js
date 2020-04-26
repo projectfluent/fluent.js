6cd2071ebca4d778e37bc8347a189bbb547d2d3a
macabeus <bruno.macabeus@gmail.com>
2020-04-26 21:18:08 +0100

    (@fluent/react) Add useLocalization tests

---
+++ fluent-react/test/use_localization.test.js
@@ -0,0 +1,57 @@
+import React from "react";
+import TestRenderer from "react-test-renderer";
+import { FluentBundle, FluentResource } from "@fluent/bundle";
+import { ReactLocalization, LocalizationProvider, useLocalization } from "../esm/index";
+
+function DummyComponent() {
+  const { l10n } = useLocalization();
+
+  return (
+    <p>{l10n.getString('foo')}</p>
+  );
+}
+
+describe("useLocalization", () => {
+  test("render inside of a LocalizationProvider", () => {
+    const renderer = TestRenderer.create(
+      <LocalizationProvider l10n={new ReactLocalization([])}>
+        <DummyComponent />
+      </LocalizationProvider>
+    );
+    expect(renderer.toJSON()).toMatchInlineSnapshot(`
+      <p>
+        foo
+      </p>
+    `);
+  });
+
+  test("render outside of a LocalizationProvider", () => {
+    const renderer = TestRenderer.create(<DummyComponent />);
+    expect(renderer.toJSON()).toMatchInlineSnapshot(`
+      <p>
+        foo
+      </p>
+    `);
+  });
+
+  test("useLocalization should exposes getString from ReactLocalization", () => {
+    const bundle = new FluentBundle("en", { useIsolating: false });
+    bundle.addResource(
+      new FluentResource(`
+foo = FOO
+`)
+    );
+
+    const renderer = TestRenderer.create(
+      <LocalizationProvider l10n={new ReactLocalization([bundle])}>
+        <DummyComponent />
+      </LocalizationProvider>
+    );
+
+    expect(renderer.toJSON()).toMatchInlineSnapshot(`
+      <p>
+        FOO
+      </p>
+    `);
+  });
+});
