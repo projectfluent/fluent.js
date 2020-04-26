69ea517db0f9fea2e1ee96d524ff3770e625112e
macabeus <bruno.macabeus@gmail.com>
2020-04-26 19:52:52 +0100

    (@fluent/react) Remove fallbackL10n

--- fluent-react/src/useTranslate.ts
+++ fluent-react/src/useTranslate.ts
@@ -2,25 +2,12 @@ import { useContext } from "react";
 import { FluentContext } from "./context";
 import { ReactLocalization } from "./localization";
 
-// Implementation of null object pattern for ReactLocalization
-const fallbackL10n: ReactLocalization = {
-  getString: id => id,
-  getBundle: () => null,
-  bundles: [],
-  parseMarkup: null,
-  reportError: () => undefined,
-};
-
 /*
 * The `useTranslate` hook returns the FluentContext
 */
 type useTranslate = () => { l10n: ReactLocalization }
 export const useTranslate: useTranslate = () => {
   const l10n = useContext(FluentContext);
-
-  if (!l10n) {
-    return { l10n: fallbackL10n };
-  }
 
   return { l10n };
 };
