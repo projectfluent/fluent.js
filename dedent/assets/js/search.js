9fe4a3ec81c1160a685ed6de8082ad1807d4b0d2
Eemeli Aro <eemeli@mozilla.com>
2023-08-01 17:31:27 +0300

    Updates built from f0730555

--- dedent/assets/js/search.js
+++
@@ -1,1 +0,0 @@
-window.searchData = {"kinds":{"64":"Function"},"rows":[{"id":0,"kind":64,"name":"default","url":"modules.html#default","classes":"tsd-kind-function"}],"index":{"version":"2.3.9","fields":["name","parent"],"fieldVectors":[["name/0",[0,2.877]],["parent/0",[]]],"invertedIndex":[["default",{"_index":0,"name":{"0":{}},"parent":{}}]],"pipeline":[]}}
