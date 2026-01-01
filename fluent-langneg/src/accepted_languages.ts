1919229cbcfb9bc4a30b80ecbc6b3f129e102cb0
Eemeli Aro <eemeli@gmail.com>
2026-01-01 16:24:35 +0200

    style: Apply Prettier

--- fluent-langneg/src/accepted_languages.ts
+++ fluent-langneg/src/accepted_languages.ts
@@ -1,4 +1,4 @@
-function parseAcceptLanguageEntry(entry: string): { lang: string, q: number } {
+function parseAcceptLanguageEntry(entry: string): { lang: string; q: number } {
   const langWithQ = entry.split(";").map(u => u.trim());
   let q = 1.0;
   if (langWithQ.length > 1) {
@@ -15,11 +15,14 @@ export function acceptedLanguages(acceptLanguageHeader = ""): string[] {
   if (typeof acceptLanguageHeader !== "string") {
     throw new TypeError("Argument must be a string");
   }
-  const tokens = acceptLanguageHeader.split(",").map(t => t.trim())
+  const tokens = acceptLanguageHeader
+    .split(",")
+    .map(t => t.trim())
     .filter(t => t !== "");
   const langsWithQ = Array.from(tokens.map(parseAcceptLanguageEntry).entries());
   // order by q descending, keeping the header order for equal weights
   langsWithQ.sort(([aidx, aval], [bidx, bval]) =>
-    aval.q === bval.q ? aidx - bidx : bval.q - aval.q);
+    aval.q === bval.q ? aidx - bidx : bval.q - aval.q
+  );
   return langsWithQ.map(([, val]) => val.lang);
 }
--- fluent-langneg/test/headers_test.js
+++ fluent-langneg/test/headers_test.js
@@ -18,50 +18,33 @@ suite("parse headers", () => {
     );
   });
 
-  test('with out of order quality values', () => {
+  test("with out of order quality values", () => {
     assert.deepStrictEqual(
-      acceptedLanguages('en;q=0.8, fr;q=0.9, de;q=0.7, *;q=0.5, fr-CH'), [
-        'fr-CH',
-        'fr',
-        'en',
-        'de',
-        '*'
-      ]
+      acceptedLanguages("en;q=0.8, fr;q=0.9, de;q=0.7, *;q=0.5, fr-CH"),
+      ["fr-CH", "fr", "en", "de", "*"]
     );
   });
 
-  test('with equal q values', () => {
+  test("with equal q values", () => {
     assert.deepStrictEqual(
-      acceptedLanguages('en;q=0.1, fr;q=0.1, de;q=0.1, *;q=0.1'), [
-        'en',
-        'fr',
-        'de',
-        '*'
-      ]
+      acceptedLanguages("en;q=0.1, fr;q=0.1, de;q=0.1, *;q=0.1"),
+      ["en", "fr", "de", "*"]
     );
   });
 
-  test('with duff q values', () => {
+  test("with duff q values", () => {
     assert.deepStrictEqual(
-      acceptedLanguages('en;q=no, fr;z=0.9, de;q=0.7;q=9, *;q=0.5, fr-CH;q=a=0.1'), [
-        'fr',
-        'fr-CH',
-        'de',
-        '*',
-        'en'
-      ]
+      acceptedLanguages(
+        "en;q=no, fr;z=0.9, de;q=0.7;q=9, *;q=0.5, fr-CH;q=a=0.1"
+      ),
+      ["fr", "fr-CH", "de", "*", "en"]
     );
   });
 
-  test('with empty entries', () => {
+  test("with empty entries", () => {
     assert.deepStrictEqual(
-      acceptedLanguages('en;q=0.8,,, fr;q=0.9,, de;q=0.7, *;q=0.5, fr-CH'), [
-        'fr-CH',
-        'fr',
-        'en',
-        'de',
-        '*'
-      ]
+      acceptedLanguages("en;q=0.8,,, fr;q=0.9,, de;q=0.7, *;q=0.5, fr-CH"),
+      ["fr-CH", "fr", "en", "de", "*"]
     );
   });
 
